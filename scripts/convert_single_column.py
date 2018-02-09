f8f4e5636c16b54ab9e8f1c4b4371fe3e3de45ba
Michael Lange <Michael.Lange@ecmwf.int>
2018-02-09 12:27:17 +0000

    OneColumn: Strip inner loop and fast array index from body

--- scripts/convert_single_column.py
+++ scripts/convert_single_column.py
@@ -1,5 +1,6 @@
 import click as cli
 import re
+from collections import OrderedDict
 
 from ecir import FortranSourceFile
 
@@ -11,10 +12,11 @@ def convert(file, output):
     print('Processing %s ...' % file)
     source = FortranSourceFile(file)
 
-    target = 'KLON'  # Name of the target dimension
+    tdim = 'KLON'  # Name of the target dimension
+    tvar = 'JL'  # Name of the target iteration variable
 
     # Get list of all declarations involving the target dimension
-    decls = [l for l in source.declarations.longlines if target in l]
+    decls = [l for l in source.declarations.longlines if tdim in l]
     decls = [l for l in decls if not l.startswith('!')]  # Strip comments
 
     # Extract names of all variables that have the target dimensions
@@ -23,11 +25,26 @@ def convert(file, output):
 
     # Strip the target dimension from all declarations
     # Note: We assume that KLON is always the leading dimension(!)
-    newdecls = [d.replace('(KLON,', '(') for d in decls]
-    newdecls = [d.replace('(KLON)', '') for d in newdecls]
-    
+    newdecls = [d.replace('(%s,' % tdim, '(') for d in decls]
+    newdecls = [d.replace('(%s)' % tdim, '') for d in newdecls]
     source.declarations.replace(dict(zip(decls, newdecls)))
-    # from IPython import embed; embed()
+
+    # Replace all target iteration indices
+    index_mapping = OrderedDict()
+    for var in vnames:
+        index_mapping.update({'(%s,' % tvar: '(', '(%s)' % tvar: ''})
+    source.body.replace(index_mapping)
+
+    # Super-hacky regex replacement for the target loops,
+    # assuming that we only ever replace the inner (fast) loop!
+    re_target_loop = re.compile('[^\n]*DO %s.*?ENDDO' % tvar, re.DOTALL)
+    re_loop_body = re.compile('DO %s.*?\n(.*?)\n\W*ENDDO' % tvar, re.DOTALL)
+    for loop in re_target_loop.findall(source.body._source):
+        # Get loop body and drop two leading chars for unindentation
+        body = re_loop_body.findall(loop)[0]
+        body = '\n'.join([line.replace('  ', '', 1) for line in body.split('\n')])
+        # Manually perform the replacement, as we're going accross lines
+        source.body._source = source.body._source.replace(loop, body)
 
     print("Writing to %s" % output)
     source.write(output)
