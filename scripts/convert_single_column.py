94437275f11126e748e180725e679495c0b61261
Michael Lange <Michael.Lange@ecmwf.int>
2018-02-15 12:21:51 +0000

    SingleClolumn: Better and conditional signature stripping

--- scripts/convert_single_column.py
+++ scripts/convert_single_column.py
@@ -26,13 +26,25 @@ class FindLoops(GenericVisitor):
             return ()
 
 
+def remove_dummy_variables(signature, variables):
+    re_sig_args = re.compile('\((?P<args>.*?)\)', re.DOTALL)
+    sig_args = re_sig_args.search(signature).groupdict()['args']
+    arguments = sig_args.split(',')
+    for var in variables:
+        for arg in arguments:
+            if var == arg.strip():
+                arguments.remove(arg)
+    return signature.replace(sig_args, ','.join(arguments))
+
+
 @cli.command()
 @cli.option('--source', '-s', help='Source file to convert.')
 @cli.option('--source-out', '-so', help='Path for generated source output.')
 @cli.option('--driver', '-d', default=None, help='Driver file to convert.')
 @cli.option('--driver-out', '-do', default=None, help='Path for generated driver output.')
 @cli.option('--mode', '-m', type=cli.Choice(['onecol', 'claw']), default='onecol')
-def convert(source, source_out, driver, driver_out, mode):
+@cli.option('--strip-signature/--no-strip-signature', default=True)
+def convert(source, source_out, driver, driver_out, mode, strip_signature):
 
     f_source = FortranSourceFile(source)
     routine = f_source.routines[0]
@@ -40,11 +52,12 @@ def convert(source, source_out, driver, driver_out, mode):
     tdim = 'KLON'  # Name of the target dimension
     tvar = 'JL'  # Name of the target iteration variable
 
-    dummy_variables = ['KIDIA', 'KFDIA', 'KLON']
+    dummy_variables = ['KIDIA', 'KFDIA', 'KLON']  # Variables to strip from signatures
 
-    # First, let's strip the target loops. It's important to do this
-    # first, as the IR on the `routine` object is not updated when the
-    # source changes...
+    ####  Remove target loops  ####
+
+    # It's important to do this first, as the IR on the `routine`
+    # object is not updated when the source changes...
     # TODO: Fully integrate IR with source changes...
     finder = FindLoops(target_var=tvar)
     for loop in flatten(routine._ir):
@@ -54,6 +67,22 @@ def convert(source, source_out, driver, driver_out, mode):
             lines = target._source.splitlines(keepends=True)[1:-1]
             lines = ''.join([line.replace('  ', '', 1) for line in lines])
             routine.body._source = routine.body._source.replace(target._source, lines)
+
+    ####  Signature adjustments  ####
+
+    if strip_signature:
+        # Strip dummy variables from signature
+        re_sig = re.compile('SUBROUTINE\s+%s.*?\(.*?\)' % routine.name, re.DOTALL)
+        signature = re_sig.findall(routine._source)[0]
+        new_signature = remove_dummy_variables(signature, dummy_variables)
+        routine.declarations._source = routine.declarations._source.replace(signature, new_signature)
+
+        # Strip dummy variables from declarations
+        for v in routine.arguments:
+            if v.name in dummy_variables:
+                routine.declarations._source = routine.declarations._source.replace(v._line, '')
+
+    ####  Index replacements  ####
 
     # Strip all target iteration indices
     routine.body.replace({'(%s,' % tvar: '(', '(%s)' % tvar: ''})
@@ -82,7 +111,8 @@ def convert(source, source_out, driver, driver_out, mode):
         if v.allocatable:
             routine.declarations.replace({'%s(:,' % v.name: '%s(' % v.name})
 
-    # Hacks, hacks, nothing but hacks... (for CUADJTQ and CLOUDSC)
+    ####  Hacks that for specific annoyances in the CLOUDSC dwarf  ####
+
     variables = [v for v in routine.variables
                  if 'KFDIA' in ','.join(v.dimensions)
                  or 'KLON' in ','.join(v.dimensions)]
@@ -100,28 +130,16 @@ def convert(source, source_out, driver, driver_out, mode):
                               '%s(KIDIA:KFDIA)' % v.name: '%s' % v.name,
                               '%s(KIDIA,' % v.name: '%s(' % v.name,
                               '%s(KIDIA)' % v.name: '%s' % v.name,
-                              'Z_TMPK(1,JK)': 'Z_TMPK(JK)'  # <= Dirty little hobbitses!!!
                          })
+    # And finally we have no shame left... :(
+    routine.body.replace({'Z_TMPK(1,JK)': 'Z_TMPK(JK)',
+                          '& (KIDIA,    KFDIA,   KLON,     KLEV,    IK,&':
+                          '& (    1,        1,      1,     KLEV,    IK,&',
+                          'JLEN=KFDIA-KIDIA+1': 'JLEN=1',
+                          'KFDIA-KIDIA+1)': '1)',
+                      })
 
-    # Strip dummy variables from signature
-    re_sig = re.compile('SUBROUTINE\s+%s.*?\(.*?\)' % routine.name, re.DOTALL)
-    signature = re_sig.findall(routine._source)[0]
-    new_signature = signature[:]
-    re_sig_args = re.compile('\((?P<args>.*?)\)', re.DOTALL)
-    sig_args = re_sig_args.search(signature).groupdict()['args']
-    dummy_args = sig_args.split(',')
-    for dummy in dummy_variables:
-        for arg in dummy_args:
-            if dummy in arg:
-                dummy_args.remove(arg)
-    # Reconstruct args in new_signature and replace in the routine source
-    new_signature = new_signature.replace(sig_args, ','.join(dummy_args))
-    routine.declarations._source = routine.declarations._source.replace(signature, new_signature)
-
-    # Strip dummy variables from declarations
-    for v in routine.arguments:
-        if v.name in dummy_variables:
-            routine.declarations._source = routine.declarations._source.replace(v._line, '')
+    ####  CLAW-specific modifications  ####
 
     if mode == 'claw':
         # Prepend CLAW directives to subroutine body
@@ -139,6 +157,12 @@ def convert(source, source_out, driver, driver_out, mode):
     if driver is not None:
         f_driver = FortranSourceFile(driver)
 
+        # # Process individual calls to our target routine
+        # re_call = re.compile('CALL %s[\s\&\(].*?\)\s*?\n' % routine.name, re.DOTALL)
+        # for call in re_call.findall(f_driver._raw_source):
+        #     # Create the outer loop from the first two arguments
+        #     pass
+            
         print("Writing to %s" % driver_out)
         f_driver.write(driver_out)
 
