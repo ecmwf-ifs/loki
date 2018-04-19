1921720d62b0a7fd18aa750b12354095e3ef38b2
Michael Lange <Michael.Lange@ecmwf.int>
2018-04-19 09:11:04 +0100

    Subroutine: Remove now obsolete `Section` concept for string replacements

--- loki/subroutine.py
+++ loki/subroutine.py
@@ -9,7 +9,7 @@ from loki.tools import flatten
 from loki.preprocessing import blacklist
 
 
-__all__ = ['Section', 'Subroutine', 'Module']
+__all__ = ['Subroutine', 'Module']
 
 
 class InterfaceBlock(object):
@@ -21,51 +21,7 @@ class InterfaceBlock(object):
         self.declarations = declarations
 
 
-class Section(object):
-    """
-    Class to handle and manipulate a source code section.
-
-    :param name: Name of the source section.
-    :param source: String with the contained source code.
-    """
-
-    def __init__(self, name, source):
-        self.name = name
-
-        self._source = source
-
-    @property
-    def source(self):
-        """
-        The raw source code contained in this section.
-        """
-        return self._source
-
-    @property
-    def lines(self):
-        """
-        Sanitizes source content into long lines with continuous statements.
-
-        Note: This does not change the content of the file
-        """
-        return self._source.splitlines(keepends=True)
-
-    def replace(self, repl, new=None):
-        """
-        Performs a line-by-line string-replacement from a given mapping
-
-        Note: The replacement is performed on each raw line. Might
-        need to improve this later to unpick linebreaks in the search
-        keys.
-        """
-        if isinstance(repl, Mapping):
-            for old, new in repl.items():
-                self._source = self._source.replace(old, new)
-        else:
-            self._source = self._source.replace(repl, new)
-
-
-class Module(Section):
+class Module(object):
     """
     Class to handle and manipulate source modules.
 
@@ -127,7 +83,7 @@ class Module(Section):
         return {td.name.upper(): td for td in types}
 
 
-class Subroutine(Section):
+class Subroutine(object):
     """
     Class to handle and manipulate a single subroutine.
 
@@ -147,19 +103,6 @@ class Subroutine(Section):
         # The actual lines in the source for this subroutine
         # TODO: Turn Section._source into a real `Source` object
         self._source = extract_source(self._ast.attrib, raw_source).string
-
-        # Separate body and declaration sections
-        # Note: The declaration includes the SUBROUTINE key and dummy
-        # variable list, so no _pre section is required.
-        body_ast = self._ast.find('body')
-        bend = int(body_ast.attrib['line_end'])
-        spec_ast = self._ast.find('body/specification')
-        sstart = int(spec_ast.attrib['line_begin']) - 1
-        send = int(spec_ast.attrib['line_end'])
-        self.header = Section(name='header', source=''.join(self.lines[:sstart]))
-        self.declarations = Section(name='declarations', source=''.join(self.lines[sstart:send]))
-        self.body = Section(name='body', source=''.join(self.lines[send:bend]))
-        self._post = Section(name='post', source=''.join(self.lines[bend:]))
 
         # Create a IRs for declarations section and the loop body
         self._ir = generate(self._ast.find('body'), self._raw_source)
@@ -199,14 +142,6 @@ class Subroutine(Section):
                 alloc = [a for a in allocs if a.variable.name == v.name]
                 if len(alloc) > 0:
                     v.dimensions = alloc[0].variable.dimensions
-
-    @property
-    def source(self):
-        """
-        The raw source code contained in this section.
-        """
-        content = [self.header, self.declarations, self.body, self._post]
-        return ''.join(s.source for s in content)
 
     @property
     def ir(self):
