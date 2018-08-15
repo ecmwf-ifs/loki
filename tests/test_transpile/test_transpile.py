6eeb3b50e59a2bf45b94b8015da3514a0eeae3dc
Michael Lange <Michael.Lange@ecmwf.int>
2018-08-15 14:08:52 +0100

    C-transpiler: Add builder and wrapper code to the transpile test

--- tests/test_transpile/test_transpile.py
+++ tests/test_transpile/test_transpile.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from pathlib import Path
 
-from loki import clean, compile_and_load, SourceFile, Module, fgen, OFP, OMNI
+from loki import clean, compile_and_load, SourceFile, Module, fgen, cgen, OMNI, Builder
 from conftest import generate_identity
 
 
@@ -22,12 +22,28 @@ def reference(refpath):
 
 def c_transpile(routine, refpath):
     """
-    Generate the C-wrapper with ISO-C bindings
+    Generate the ISO-C bindings wrapper and C-transpiled source code
     """
+    path = refpath.parent
+    builder = Builder(source_dirs=path, build_dir=path/'build')
+    builder.clean()
+
+    # Generate Fortran wrapper module
     wrapper = routine.generate_iso_c_wrapper(suffix='_iso_c')
-    wrapperpath = (refpath.parent/wrapper.name).with_suffix('.f90')
+    wrapperpath = (path/wrapper.name).with_suffix('.f90')
     SourceFile.to_file(source=fgen(wrapper), path=wrapperpath)
-    return compile_and_load(wrapperpath, cwd=str(refpath.parent))
+
+    # Generate C source file from Loki IR
+    routine.name = '%s_c' % routine.name
+    c_path = (path/routine.name).with_suffix('.c')
+    SourceFile.to_file(source=cgen(routine), path=c_path)
+
+    # Build and wrap the cross-compiled library
+    builder = Builder(source_dirs=path, build_dir=path/'build')
+    lib = builder.Lib(name='fclib', objects=[wrapperpath.name, c_path.name])
+    lib.build()
+
+    return lib.wrap(modname='fcmod', sources=[wrapperpath.name])
 
 
 def test_transpile_simple_loops(refpath, reference):
