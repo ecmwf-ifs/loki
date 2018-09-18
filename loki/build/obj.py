8c0aa89d75fee363e77d3c09bcf741c97bd90db6
Michael Lange <Michael.Lange@ecmwf.int>
2018-09-18 11:04:08 +0100

    Obj: Another tweak to the `USE` dependency regex

--- loki/build/obj.py
+++ loki/build/obj.py
@@ -12,9 +12,7 @@ from loki.build.compiler import _default_compiler
 __all__ = ['Obj']
 
 
-_str_no_comment = '^\s*[^C!][^!]'
-
-_re_use = re.compile(_str_no_comment+'use\s+(?P<use>\w+)', re.IGNORECASE | re.MULTILINE)
+_re_use = re.compile('^\s*use\s+(?P<use>\w+)', re.IGNORECASE | re.MULTILINE)
 _re_include = re.compile('\#include\s+["\']([\w\.]+)[\"\']', re.IGNORECASE)
 # Please note that the below regexes are fairly expensive due to .* with re.DOTALL
 _re_module = re.compile('module\s+(\w+).*end module', re.IGNORECASE | re.DOTALL)
