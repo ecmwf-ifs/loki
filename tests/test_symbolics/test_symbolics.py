f409517fa50a50d203480a8caa5cb6b72ae996c2
Michael Lange <Michael.Lange@ecmwf.int>
2019-01-02 15:45:12 +0000

    Symbolics: Add tests for symbolic inlinecalls and casts

--- tests/test_symbolics/test_symbolics.py
+++ tests/test_symbolics/test_symbolics.py
@@ -1,6 +1,6 @@
 from sympy import symbols, simplify
 
-from loki import Variable, Scalar, Subroutine
+from loki import Variable, Scalar, Subroutine, InlineCall, Cast, fsymgen, BaseType
 
 
 def test_symbolic_equivalence():
@@ -72,7 +72,7 @@ def test_symbol_caching_kernel():
     assert f2 + 1 == 1 + f3
 
 
-def test_symbol_regeneration():
+def test_symbol_regenerate_variable():
     """
     Test symbols can be re-created by SymPy for simplification.
     """
@@ -90,3 +90,63 @@ def test_symbol_regeneration():
     # Force simplification to trigger symbol re-generation
     assert 3*f == simplify(f + 2*f)
     assert 3*g == simplify(g + 2*g)
+
+
+def test_symbol_regenerate_inlinecall():
+    """
+    Test custom symbols can be re-created by SymPy for simplification.
+    """
+    x = Variable(name='x')
+    y = Variable(name='y')
+    f = Variable(name='f', dimensions=(x, y))
+    g = Variable(name='g')
+
+    a = InlineCall('a', arguments=(f, g))
+    b = InlineCall(name='b', arguments=(f, g), kwarguments=(('x', x), ('y', y)))
+
+    # Sanity check the SymPy-style class markers
+    assert not a.is_Symbol and not a.is_Function
+    assert not b.is_Symbol and not b.is_Function
+
+    # Force simplification to trigger symbol re-generation
+    assert 3*a == simplify(a + 2*a)
+    assert 3*b == simplify(b + 2*b)
+
+    # And finally check correct Fortran printing
+    assert fsymgen(a) == 'a(f(x, y), g)'
+    assert fsymgen(b) == 'b(f(x, y), g, x=x, y=y)'
+
+
+def test_symbol_regenerate_cast():
+    """
+    Test custom symbols can be re-created by SymPy for simplification.
+    """
+    x = Variable(name='x')
+    y = Variable(name='y')
+    f = Variable(name='f', dimensions=(x, y))
+    g = Variable(name='g')
+
+    a = Cast(name='real', expression=x + y)
+    b = Cast(name='real', expression=x + y, kind='JPRB')
+    kind = InlineCall(name='selected_real_kind', arguments=(13, 300))
+    c = Cast(name='real', expression=x + y, kind=kind)
+
+    # Sanity check the SymPy-style class markers
+    assert not a.is_Symbol and not a.is_Function
+    assert not b.is_Symbol and not b.is_Function
+    assert not c.is_Symbol and not c.is_Function
+
+    # Force simplification to trigger symbol re-generation
+    assert 3*a == simplify(a + 2*a)
+    assert 3*b == simplify(b + 2*b)
+    assert 3*c == simplify(c + 2*c)
+
+    # Check correct pretty-printing
+    assert str(a) == 'real(x + y)'
+    assert str(b) == 'real(x + y, kind=JPRB)'
+    assert str(c) == 'real(x + y, kind=selected_real_kind(13, 300))'
+
+    # And finally check correct Fortran printing
+    assert fsymgen(a) == 'real(x + y)'
+    assert fsymgen(b) == 'real(x + y, kind=JPRB)'
+    assert fsymgen(c) == 'real(x + y, kind=selected_real_kind(13, 300))'
