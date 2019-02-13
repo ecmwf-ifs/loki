8d13155343d8828db17a49677c4b09f635342f0e
Michael Lange <Michael.Lange@ecmwf.int>
2019-02-13 11:36:57 +0000

    TWEAK: test for second-level caching...

--- tests/test_symbolics/test_symbolics.py
+++ tests/test_symbolics/test_symbolics.py
@@ -1,6 +1,7 @@
 from sympy import symbols, simplify
 
-from loki import Variable, Scalar, Array, Subroutine, InlineCall, Cast, fsymgen, BaseType, indexify
+from loki import (Variable, Scalar, Array, Subroutine, InlineCall, Cast, fsymgen,
+                  BaseType, indexify, SymbolCache)
 
 
 def test_symbolic_equivalence():
@@ -88,6 +89,57 @@ def test_symbol_regenerate_variable():
     # Force simplification to trigger symbol re-generation
     assert 3*f == simplify(f + 2*f)
     assert 3*g == simplify(g + 2*g)
+
+
+def test_symbol_regenerate_meta_info():
+    """
+    Test symbols can be re-created by SymPy for simplification.
+    """
+    x = Variable(name='x')
+    y = Variable(name='y')
+    i = Variable(name='i')
+    j = Variable(name='j')
+    f = Variable(name='f', dimensions=(i, j), shape=(x, y))
+
+    # Sanity check meta-info before simplification
+    assert f.shape == (x, y)
+
+    # Force simplification, but leave variable intact
+    g = simplify(f + 2*f)
+    assert g == 3*f
+    assert g.args[1] == f
+    assert g.args[1].shape == (x, y)
+
+    # Replace index on variable and checl shape
+    h = f.xreplace({j: i})
+    assert h == f.func(i, i)
+    assert h.shape == (x, y)
+
+
+def test_symbol_regenerate_meta_from_cache():
+    """
+    Test symbols can be re-created by SymPy for simplification.
+    """
+    cache = SymbolCache()
+    x = cache.Variable(name='x')
+    y = cache.Variable(name='y')
+    i = cache.Variable(name='i')
+    j = cache.Variable(name='j')
+    f = cache.Variable(name='f', dimensions=(i, j), shape=(x, y))
+
+    # Sanity check meta-info before simplification
+    assert f.shape == (x, y)
+
+    # Force simplification, but leave variable intact
+    g = simplify(f + 2*f)
+    assert g == 3*f
+    assert g.args[1] == f
+    assert g.args[1].shape == (x, y)
+
+    # Replace index on variable and checl shape
+    h = f.xreplace({j: i})
+    assert h == f.func(i, i)
+    assert h.shape == (x, y)
 
 
 def test_symbol_regenerate_inlinecall():
