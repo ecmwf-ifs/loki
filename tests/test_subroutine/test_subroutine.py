05fc45dc2a527cb79238eabc4004ede65a50b3f0
Michael Lange <Michael.Lange@ecmwf.int>
2019-10-11 15:50:22 +0100

    OMNI: Fix failing `test_subroutine` due to missing `header.xmod`
    Not pretty, I know. Hopefully will be fixed in a better way soon...

--- tests/test_subroutine/test_subroutine.py
+++ tests/test_subroutine/test_subroutine.py
@@ -35,10 +35,13 @@ def reference(refpath, header_mod):
 
 
 @pytest.mark.parametrize('frontend', [OFP, OMNI])
-def test_routine_simple(refpath, reference, frontend):
+def test_routine_simple(refpath, reference, header_path, frontend):
     """
     A simple standard looking routine to test argument declarations.
     """
+    # FIXME: Forces pre-parsing of header module for OMNI parser to generate .xmod!
+    _ = SourceFile.from_file(header_path, frontend=frontend)['header']
+
     # Test the internals of the :class:`Subroutine`
     routine = SourceFile.from_file(refpath, frontend=frontend)['routine_simple']
     routine_args = [str(arg) for arg in routine.arguments]
@@ -58,10 +61,14 @@ def test_routine_simple(refpath, reference, frontend):
 
 
 @pytest.mark.parametrize('frontend', [OFP, OMNI])
-def test_routine_simple_caching(refpath, reference, frontend):
+def test_routine_simple_caching(refpath, reference, header_path, frontend):
     """
     A simple standard looking routine to test variable caching.
     """
+
+    # FIXME: Forces pre-parsing of header module for OMNI parser to generate .xmod!
+    _ = SourceFile.from_file(header_path, frontend=frontend)['header']
+
     # Test the internals of the :class:`Subroutine`
     routine = SourceFile.from_file(refpath, frontend=frontend)['routine_simple']
     routine_args = [str(arg) for arg in routine.arguments]
@@ -80,10 +87,13 @@ def test_routine_simple_caching(refpath, reference, frontend):
 
 
 @pytest.mark.parametrize('frontend', [OFP, OMNI])
-def test_routine_multiline_args(refpath, reference, frontend):
+def test_routine_multiline_args(refpath, reference, header_path, frontend):
     """
     A simple standard looking routine to test argument declarations.
     """
+    # FIXME: Forces pre-parsing of header module for OMNI parser to generate .xmod!
+    _ = SourceFile.from_file(header_path, frontend=frontend)['header']
+
     # Test the internals of the :class:`Subroutine`
     routine = SourceFile.from_file(refpath, frontend=frontend)['routine_multiline_args']
     routine_args = [str(arg) for arg in routine.arguments]
@@ -103,10 +113,13 @@ def test_routine_multiline_args(refpath, reference, frontend):
 
 
 @pytest.mark.parametrize('frontend', [OFP, OMNI])
-def test_routine_local_variables(refpath, reference, frontend):
+def test_routine_local_variables(refpath, reference, header_path, frontend):
     """
     Test local variables and types
     """
+    # FIXME: Forces pre-parsing of header module for OMNI parser to generate .xmod!
+    _ = SourceFile.from_file(header_path, frontend=frontend)['header']
+
     # Test the internals of the :class:`Subroutine`
     routine = SourceFile.from_file(refpath, frontend=frontend)['routine_local_variables']
     routine_vars = [str(arg) for arg in routine.variables]
@@ -121,10 +134,12 @@ def test_routine_local_variables(refpath, reference, frontend):
 
 
 @pytest.mark.parametrize('frontend', [OFP, OMNI])
-def test_routine_arguments(refpath, reference, frontend):
+def test_routine_arguments(refpath, reference, header_path, frontend):
     """
     A set of test to test internalisation and handling of arguments.
     """
+    # FIXME: Forces pre-parsing of header module for OMNI parser to generate .xmod!
+    _ = SourceFile.from_file(header_path, frontend=frontend)['header']
 
     routine = SourceFile.from_file(refpath, frontend=frontend)['routine_arguments']
     routine_vars = [str(arg) for arg in routine.variables]
@@ -149,10 +164,13 @@ def test_routine_arguments(refpath, reference, frontend):
 
 
 @pytest.mark.parametrize('frontend', [OFP, OMNI])
-def test_find_variables(refpath, reference, frontend):
+def test_find_variables(refpath, reference, header_path, frontend):
     """
     Tests the `FindVariables` utility (not the best place to put this).
     """
+    # FIXME: Forces pre-parsing of header module for OMNI parser to generate .xmod!
+    _ = SourceFile.from_file(header_path, frontend=frontend)['header']
+
     routine = SourceFile.from_file(refpath, frontend=frontend)['routine_local_variables']
 
     vars_all = FindVariables(unique=False).visit(routine.ir)
@@ -178,11 +196,14 @@ def test_find_variables(refpath, reference, frontend):
 
 
 @pytest.mark.parametrize('frontend', [OFP, OMNI])
-def test_routine_dim_shapes(refpath, reference, frontend):
+def test_routine_dim_shapes(refpath, reference, header_path, frontend):
     """
     A set of test to ensure matching different dimension and shape
     expressions against strings and other expressions works as expected.
     """
+    # FIXME: Forces pre-parsing of header module for OMNI parser to generate .xmod!
+    _ = SourceFile.from_file(header_path, frontend=frontend)['header']
+
     # TODO: Need a named subroutine lookup
     routine = SourceFile.from_file(refpath, frontend=frontend)['routine_dim_shapes']
     routine_args = [str(arg) for arg in routine.arguments]
