9fb998462c09cacadfce1855f4a522664bbdb5d8
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2020-04-02 20:45:57 +0100

    Bugfixes for the linter config and when determining the list of rules

--- loki/lint/linter.py
+++ loki/lint/linter.py
@@ -3,7 +3,6 @@ import time
 
 from loki.lint.reporter import FileReport, RuleReport
 from loki.sourcefile import SourceFile
-from loki import logger
 
 
 class Linter(object):
@@ -15,13 +14,12 @@ class Linter(object):
             rule_names = [rule.__name__ for rule in rules]
         elif config is not None:
             rule_names = config.get('rules') if config is not None else None
-        self.rules = rules if rules is not None else Linter._lookup_rules(rule_names)
+        self.rules = rules if rules is not None else Linter.lookup_rules(rule_names)
         self.config = self.default_config(self.rules)
-        if config is not None:
-            self.config.update(config)
+        self.update_config(config)
 
     @staticmethod
-    def _lookup_rules(rule_names=None):
+    def lookup_rules(rule_names=None):
         import loki.lint.rules as rules
         rule_list = inspect.getmembers(
             rules, lambda obj: inspect.isclass(obj) and obj.__name__.endswith('Rule'))
@@ -34,13 +32,23 @@ class Linter(object):
     def default_config(rules=None):
         config = {}
         if rules is None:
-            rules = Linter._lookup_rules()
+            rules = Linter.lookup_rules()
         # List of rules
         config['rules'] = [rule.__name__ for rule in rules]
         # Default options for rules
         for rule in rules:
             config[rule.__name__] = rule.config
         return config
+
+    def update_config(self, config):
+        if config is None:
+            return
+        for key, val in config.items():
+            # If we have a dict, update that entry
+            if isinstance(val, dict) and key in self.config:
+                self.config[key].update(val)
+            else:
+                self.config[key] = val
 
     def check(self, sourcefile, overwrite_rules=None, overwrite_config=None):
         if not isinstance(sourcefile, SourceFile):
