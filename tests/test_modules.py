44d837093b0c5bfc5b4763a9210ef70c11b6569a
Michael Lange <Michael.Lange@ecmwf.int>
2020-05-07 08:24:11 +0100

    Modules: Add test for `typedefs` injection in `Module` constructor
    Thie test that nested type infor from an external derived type gets
    attached to an enclosing derived type in the module and propagated
    to the usage of that variable in a module subroutine.

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pytest
 
-from loki import OFP, OMNI, FP, Module, Declaration, TypeDef
+from loki import OFP, OMNI, FP, Module, Declaration, TypeDef, DataType
 
 
 @pytest.mark.parametrize('frontend', [FP, OFP, OMNI])
@@ -31,3 +31,71 @@ end module a_module
     assert 'derived_type' in module.typedefs
     assert len(module.routines) == 1
     assert module.routines[0].name == 'my_routine'
+
+
+@pytest.mark.parametrize('frontend', [FP, OFP, OMNI])
+def test_module_external_typedefs(frontend):
+    """
+    Test that externally provided type information is correctly
+    attached to `Module` components when supplied via the `typedefs`
+    parameter in the constructor.
+    """
+    fcode_external = """
+module external_mod
+  integer, parameter :: x = 2
+  integer, parameter :: y = 3
+
+  type ext_type
+    real :: array(x, y)
+  end type ext_type
+end module external_mod
+"""
+
+    fcode_module = """
+module a_module
+  use external_mod, only: ext_type
+  implicit none
+
+  type nested_type
+    type(ext_type) :: ext
+  end type nested_type
+contains
+
+  subroutine my_routine(pt)
+    type(nested_type) :: pt
+    pt%ext%array(:,:) = 42.0
+  end subroutine my_routine
+end module a_module
+"""
+
+    external = Module.from_source(fcode_external, frontend=frontend)
+    assert'ext_type' in external.typedefs
+
+    module = Module.from_source(fcode_module, frontend=frontend,
+                                typedefs=external.typedefs)
+    nested = module.typedefs['nested_type']
+    ext = nested.variables[0]
+
+    # Check that the `array` variable in the `ext` type is found and
+    # has correct type and shape info
+    assert 'array' in ext.type.variables
+    a = ext.type.variables['array']
+    assert a.type.dtype == DataType.REAL
+    assert a.shape[0].name == 'x'
+    assert a.shape[1].name == 'y'
+
+    # Check the routine has got type and shape info too
+    routine = module['my_routine']
+    pt = routine.variables[0]
+    pt_ext = pt.type.variables['ext']
+    assert 'array' in pt_ext.type.variables
+    pt_ext_a = pt_ext.type.variables['array']
+    assert pt_ext_a.type.dtype == DataType.REAL
+    assert pt_ext_a.shape[0].name == 'x'
+    assert pt_ext_a.shape[1].name == 'y'
+
+    # Check the LHS of the assignment has correct meta-data
+    pt_ext_arr = routine.body[0].target
+    assert pt_ext_arr.type.dtype == DataType.REAL
+    assert pt_ext_arr.shape[0].name == 'x'
+    assert pt_ext_arr.shape[1].name == 'y'
