afbec62b6370ac3a76f62ccf80a9889e0ec23ac6
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2020-06-09 20:17:21 +0100

    Some cleanup in visitors

--- loki/visitors.py
+++ loki/visitors.py
@@ -286,7 +286,7 @@ class Stringifier(Visitor):
             # onto the line and have enough space left for a line break
             new_line = line + item
             if len(new_line) + len(self.cont[0]) <= self.width:
-               return new_line, []
+                return new_line, []
 
             # Putting the current item plus separator and potential line break
             # onto the current line exceeds the allowed width: we need to break.
@@ -304,16 +304,18 @@ class Stringifier(Visitor):
             chunks[:-1] = [chunk + ' ' for chunk in chunks[:-1]]
 
             # First, add as much as possible to the previous line
+            next_chunk = 0
             for idx, chunk in enumerate(chunks):
                 new_line = line + chunk
                 if len(new_line) + len(self.cont[0]) > self.width:
+                    next_chunk = idx
                     break
                 line = new_line
 
             # Now put the rest on new lines
             lines = [line + self.cont[0]]
             line = self.cont[1]
-            for chunk in chunks[idx:]:
+            for chunk in chunks[next_chunk:]:
                 new_line = line + chunk
                 if len(new_line) + len(self.cont[0]) > self.width:
                     lines += [line + self.cont[0]]
@@ -525,150 +527,5 @@ class Stringifier(Visitor):
         return self.join_lines(header, *body)
 
 
-class PrintAST(Visitor):
-    # pylint: disable=no-self-use,unused-argument
-
-    _depth = 0
-
-    def __init__(self, verbose=True):
-        super(PrintAST, self).__init__()
-        self.verbose = verbose
-
-    @classmethod
-    def default_retval(cls):
-        return "<>"
-
-    @property
-    def indent(self):
-        return '  ' * self._depth
-
-    def visit_Node(self, o, **kwargs):
-        return self.indent + '<%s>' % o.__class__.__name__
-
-    def visit_tuple(self, o, **kwargs):
-        return '\n'.join([self.visit(i, **kwargs) for i in o])
-
-    visit_list = visit_tuple
-
-    def visit_Block(self, o, **kwargs):
-        self._depth += 2
-        body = self.visit(o.body, **kwargs)
-        self._depth -= 2
-        return self.indent + "<Block>\n%s" % body
-
-    def visit_Loop(self, o, **kwargs):
-        self._depth += 2
-        body = self.visit(o.children, **kwargs)
-        self._depth -= 2
-        if self.verbose and o.bounds is not None:
-            bounds = ' :: %s' % str(o.bounds)
-        else:
-            bounds = ''
-        return self.indent + "<Loop %s%s>\n%s" % (o.variable, bounds, body)
-
-    def visit_WhileLoop(self, o, **kwargs):
-        self._depth += 2
-        body = self.visit(o.children, **kwargs)
-        self._depth -= 2
-        if self.verbose and o.condition is not None:
-            condition = ' :: %s' % str(o.condition)
-        else:
-            condition = ''
-        return self.indent + "<WhileLoop%s>\n%s" % (condition, body)
-
-    def visit_Conditional(self, o, **kwargs):
-        self._depth += 2
-        bodies = tuple(self.visit(b, **kwargs) for b in o.bodies)
-        self._depth -= 2
-        out = self.indent + '<If %s>\n%s' % (o.conditions[0], bodies[0])
-        for b, c in zip(bodies[1:], o.conditions[1:]):
-            out += '\n%s' % self.indent + '<Else-If %s>\n%s' % (c, b)
-        if o.else_body is not None:
-            self._depth += 2
-            else_body = self.visit(o.else_body, **kwargs)
-            self._depth -= 2
-            out += '\n%s' % self.indent + '<Else>\n%s' % else_body
-        return out
-
-    def visit_Statement(self, o, **kwargs):
-        expr = (' => ' if o.ptr else ' = ') + str(o.expr) if self.verbose else ''
-        if self.verbose and o.comment is not None:
-            self._depth += 2
-            comment = '\n%s' % self.visit(o.comment, **kwargs)
-            self._depth -= 2
-        else:
-            comment = ''
-        return self.indent + '<Stmt %s%s>%s' % (str(o.target), expr, comment)
-
-    def visit_Scope(self, o, **kwargs):
-        self._depth += 2
-        body = self.visit(o.body, **kwargs)
-        self._depth -= 2
-        return self.indent + "<Scope>\n%s" % body
-
-    def visit_Declaration(self, o, **kwargs):
-        variables = ' :: %s' % ', '.join(v.name for v in o.variables) if self.verbose else ''
-        comment = ''
-        pragma = ''
-
-        if self.verbose and o.comment is not None:
-            self._depth += 2
-            comment = '\n%s' % self.visit(o.comment, **kwargs)
-            self._depth -= 2
-        if self.verbose and o.pragma is not None:
-            self._depth += 2
-            pragma = '\n%s' % self.visit(o.pragma, **kwargs)
-            self._depth -= 2
-        return self.indent + '<Declaration%s>%s%s' % (variables, comment, pragma)
-
-    def visit_Allocation(self, o, **kwargs):
-        variable = " %s" % o.variable if self.verbose else ''
-        return self.indent + '<Alloc%s>' % variable
-
-    def visit_CallStatement(self, o, **kwargs):
-        args = '(%s)' % (', '.join(str(a) for a in o.arguments)) if self.verbose else ''
-        return self.indent + '<CallStatement %s%s>' % (o.name, args)
-
-    def visit_Comment(self, o, **kwargs):
-        body = '::%s::' % o._source.string if self.verbose else ''
-        return self.indent + '<Comment%s>' % body
-
-    def visit_CommentBlock(self, o, **kwargs):
-        body = ('\n%s' % self.indent).join([b._source.string for b in o.comments])
-        return self.indent + '<CommentBlock%s' % (
-            ('\n%s' % self.indent) + body + '>' if self.verbose else '>')
-
-    def visit_Pragma(self, o, **kwargs):
-        body = ' ::%s::' % o._source.string if self.verbose else ''
-        return self.indent + '<Pragma %s%s>' % (o.keyword, body)
-
-    def visit_Variable(self, o, **kwargs):
-        dimensions = ('(%s)' % ','.join([str(v) for v in o.dimensions])) if o.dimensions else ''
-        _type = self.visit(o.type, **kwargs) if o.type is not None else ''
-        return self.indent + '<Var %s%s%s>' % (o.name, dimensions, _type)
-
-    def visit_BaseType(self, o, **kwargs):
-        ptr = ', ptr' if o.pointer else ''
-        return '<Type %s:%s%s>' % (o.name, o.kind, ptr)
-
-    def visit_DerivedType(self, o, **kwargs):
-        variables = ''
-        comments = ''
-        pragmas = ''
-        if self.verbose:
-            self._depth += 2
-            variables = '\n%s' % self.visit(o.variables, **kwargs)
-            self._depth -= 2
-        if self.verbose and o.comments is not None:
-            self._depth += 2
-            comments = '\n%s' % self.visit(o.comments, **kwargs)
-            self._depth -= 2
-        if self.verbose and o.pragmas is not None:
-            self._depth += 2
-            pragmas = '\n%s' % self.visit(o.pragmas, **kwargs)
-            self._depth -= 2
-        return self.indent + '<DerivedType %s>%s%s%s' % (o.name, variables, pragmas, comments)
-
-
-def pprint(ir, verbose=False):
-    print(PrintAST(verbose=verbose).visit(ir))
+def pprint(ir):
+    print(Stringifier().visit(ir))
