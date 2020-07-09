071648b9d1a1a2fdd62296786d9bcce2c72b3937
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2020-07-09 16:44:05 +0100

    Rewrite cgen backend using Stringifier

--- loki/backend/cgen.py
+++ loki/backend/cgen.py
@@ -1,8 +1,7 @@
 from pymbolic.mapper.stringifier import (PREC_SUM, PREC_PRODUCT, PREC_UNARY, PREC_LOGICAL_OR,
                                          PREC_LOGICAL_AND, PREC_NONE, PREC_CALL)
 
-from loki.tools import chunks
-from loki.visitors import Visitor, FindNodes, Transformer
+from loki.visitors import Stringifier, FindNodes, Transformer
 from loki.ir import Import, Declaration
 from loki.expression import LokiStringifyMapper, Array
 from loki.types import DataType, SymbolType
@@ -141,58 +140,53 @@ class CCodeMapper(LokiStringifyMapper):
             enclosing_prec, PREC_NONE)
 
 
-class CCodegen(Visitor):
+class CCodegen(Stringifier):
     """
     Tree visitor to generate standardized C code from IR.
     """
-    # pylint: disable=no-self-use
 
-    def __init__(self, depth=0, linewidth=90, chunking=6):
-        super(CCodegen, self).__init__()
-        self.linewidth = linewidth
-        self.chunking = chunking
-        self._depth = depth
-        self._csymgen = CCodeMapper()
+    def __init__(self, depth=0, indent='  ', linewidth=90):
+        super().__init__(depth=depth, indent=indent, linewidth=linewidth,
+                         line_cont=lambda indent: '\n{}  '.format(indent),
+                         symgen=CCodeMapper())
 
-    @classmethod
-    def default_retval(cls):
-        return ""
+    # Handler for outer objects
 
-    @property
-    def indent(self):
-        return '  ' * self._depth
-
-    @property
-    def csymgen(self):
-        return self._csymgen
-
-    def segment(self, arguments, chunking=None):
-        chunking = chunking or self.chunking
-        delim = ',\n%s  ' % self.indent
-        args = list(chunks(list(arguments), chunking))
-        return delim.join(', '.join(c) for c in args)
-
-    def visit_Expression(self, o, **kwargs):
-        return self.csymgen(o)
-
-    def visit_str(self, o, **kwargs):
-        return o
-
-    def visit_Node(self, o, **kwargs):
-        return self.indent + '// <%s>' % o.__class__.__name__
-
-    def visit_tuple(self, o, **kwargs):
-        return '\n'.join([self.visit(i, **kwargs) for i in o])
-
-    visit_list = visit_tuple
+    def visit_SourceFile(self, o, **kwargs):
+        """
+        Format as
+          ...modules...
+          ...subroutines...
+        """
+        modules = self.visit_all(o.modules, **kwargs)
+        subroutines = self.visit_all(o.subroutines, **kwargs)
+        return self.join_lines(*modules, *subroutines)
 
     def visit_Module(self, o, **kwargs):
         # Assuming this will be put in header files...
         spec = self.visit(o.spec, **kwargs)
         routines = self.visit(o.routines, **kwargs)
-        return spec + '\n\n' + routines
+        return self.join_lines(spec, routines)
 
     def visit_Subroutine(self, o, **kwargs):
+        """
+        Format as:
+
+          ...imports...
+          int <name>(<args>) {
+            ...spec without imports and argument declarations...
+            ...body...
+          }
+        """
+        # Some boilerplate imports...
+        standard_imports = ['stdio.h', 'stdbool.h', 'float.h', 'math.h']
+        header = [self.format_line('#include <', name, '>') for name in standard_imports]
+
+        # ...and remove imports from the spec
+        spec_imports = FindNodes(Import).visit(o.spec)
+        header += [self.visit(spec_imports, **kwargs)]
+        spec = Transformer({node: None for node in spec_imports}).visit(o.spec)
+
         # Generate header with argument signature
         aptr = []
         for a in o.arguments:
@@ -207,75 +201,196 @@ class CCodegen(Visitor):
                 aptr += ['']
         arguments = ['%s %s%s' % (self.visit(a.type, **kwargs), p, a.name.lower())
                      for a, p in zip(o.arguments, aptr)]
-        arguments = self.segment(arguments)
-        header = 'int %s(%s)\n{\n' % (o.name, arguments)
+        header += [self.format_line('int ', o.name, '(', self.join_items(arguments), ') {')]
 
-        self._depth += 1
+        self.depth += 1
 
         # Generate the array casts for pointer arguments
-        casts = '%s/* Array casts for pointer arguments */\n' % self.indent
+        body = [self.format_line('/* Array casts for pointer arguments */')]
         for a in o.arguments:
             if isinstance(a, Array):
                 dtype = self.visit(a.type, **kwargs)
                 # str(d).lower() is a bad hack to ensure caps-alignment
                 outer_dims = ''.join('[%s]' % self.visit(d, **kwargs).lower()
                                      for d in a.dimensions.index_tuple[1:])
-                casts += self.indent + '%s (*%s)%s = (%s (*)%s) v_%s;\n' % (
-                    dtype, a.name.lower(), outer_dims, dtype, outer_dims, a.name.lower())
+                body += [self.format_line(dtype, ' (*', a.name.lower(), ')', outer_dims, ' = (',
+                                          dtype, ' (*)', outer_dims, ') v_', a.name.lower(), ';')]
+#                casts += self.indent + '%s (*%s)%s = (%s (*)%s) v_%s;\n' % (
+#                    dtype, a.name.lower(), outer_dims, dtype, outer_dims, a.name.lower())
 
         # Remove declarations for arguments, as C arguments are declarations!
         decl_map = {}
-        for decl in FindNodes(Declaration).visit(o.spec):
+        for decl in FindNodes(Declaration).visit(spec):
             new_vars = [v for v in decl.variables if v not in o.arguments]
             if len(new_vars) > 0:
                 decl_map[decl] = decl.clone(variables=new_vars)
             else:
                 decl_map[decl] = None
-        o.spec = Transformer(decl_map).visit(o.spec, **kwargs)
-
-        # Some boilerplate imports...
-        imports = '#include <stdio.h>\n'  # For manual debugging
-        imports += '#include <stdbool.h>\n'
-        imports += '#include <float.h>\n'
-        imports += '#include <math.h>\n'
-        imports += self.visit(FindNodes(Import).visit(o.spec), **kwargs)
-
-        # ...remove imports from the spec...
-        import_map = {imprt: None for imprt in FindNodes(Import).visit(o.spec, **kwargs)}
-        o.spec = Transformer(import_map).visit(o.spec, **kwargs)
+        spec = Transformer(decl_map).visit(spec, **kwargs)
 
         # ...and generate the rest of spec and body
-        spec = self.visit(o.spec, **kwargs)
-        body = self.visit(o.body, **kwargs)
-        footer = '\n%sreturn 0;\n}' % self.indent
-        self._depth -= 1
+        body += [self.visit(spec, **kwargs)]
+        body += [self.visit(o.body, **kwargs)]
+        body += [self.format_line('return 0;')]
 
-        return imports + '\n\n' + header + casts + spec + '\n' + body + footer
+        # Close everything off
+        self.depth -= 1
+        footer = [self.format_line('}')]
 
-    def visit_Section(self, o, **kwargs):
-        return self.visit(o.body, **kwargs) + '\n'
+        return self.join_lines(*header, *body, *footer)
 
-    def visit_Import(self, o, **kwargs):  # pylint: disable=unused-argument
-        return ('#include "%s"' % o.module) if o.c_import else ''
+    # Handler for AST base nodes
+
+    def visit_Node(self, o, **kwargs):
+        """
+        Format non-supported nodes as
+          // <repr(Node)>
+        """
+        return self.format_line('// <', repr(o), '>')
+
+    # Handler for IR nodes
+
+    def visit_Intrinsic(self, o, **kwargs):  # pylint: disable=unused-argument
+        """
+        Format intrinsic nodes.
+        """
+        return self.format_line(str(o.text).lstrip())
+
+    def visit_Comment(self, o, **kwargs):  # pylint: disable=unused-argument
+        """
+        Format comments.
+        """
+        text = o.text or o.source.string
+        text = str(text).lstrip().replace('!', '//', 1)
+        return self.format_line(text, no_wrap=True)
+
+    def visit_CommentBlock(self, o, **kwargs):
+        """
+        Format comment blocks.
+        """
+        comments = self.visit_all(o.comments, **kwargs)
+        return self.join_lines(*comments)
 
     def visit_Declaration(self, o, **kwargs):
+        """
+        Format declaration as
+          <type> <name> [= <initial>]
+        """
         types = [v.type for v in o.variables]
         # Ensure all variable types are equal, except for shape and dimension
         ignore = ['shape', 'dimensions', 'source']
         assert all(t.compare(types[0], ignore=ignore) for t in types)
-        dtype = self.visit(types[0])
-
-        comment = '  %s' % self.visit(o.comment, **kwargs) if o.comment is not None else ''
+        dtype = self.visit(types[0], **kwargs)
+        assert len(o.variables) > 0
         variables = []
         for v in o.variables:
-            stmt = self.visit(v, **kwargs)
+            var = self.visit(v, **kwargs)
+            initial = ''
             if v.initial is not None:
-                stmt += ' = %s' % self.visit(v.initial, **kwargs)
+                initial = ' = {}'.format(self.visit(v.initial, **kwargs))
             if v.type.pointer or v.type.allocatable:
-                stmt = '*' + stmt
-            variables += [stmt]
-        variables = self.segment(variables)
-        return self.indent + '%s %s;' % (dtype, variables) + comment
+                var = '*' + var
+            variables += ['{}{}'.format(var, initial)]
+        comment = None
+        if o.comment:
+            comment = str(self.visit(o.comment, **kwargs))
+        return self.format_line(dtype, ' ', self.join_items(variables), ';', comment=comment)
+
+    def visit_Import(self, o, **kwargs):  # pylint: disable=unused-argument
+        """
+        Format C imports as
+          #include "<name>"
+        """
+        if o.c_import:
+            return self.format_line('#include "', str(o.module), '"')
+        return ''
+
+    def visit_Loop(self, o, **kwargs):
+        """
+        Format loop with explicit range as
+          for (<var>=<start>; <var><criteria><end>; <var> += <incr>) {
+            ...body...
+          }
+        """
+        control = 'for ({var} = {start}; {var} {crit} {end}; {var} += {incr})'.format(
+            var=self.visit(o.variable, **kwargs), start=self.visit(o.bounds.start, **kwargs),
+            end=self.visit(o.bounds.stop, **kwargs),
+            crit='<=' if not o.bounds.step or eval(str(o.bounds.step)) > 0 else '>=',
+            incr=self.visit(o.bounds.step, **kwargs) if o.bounds.step else 1)
+        header = self.format_line(control, ' {')
+        footer = self.format_line('}')
+        self.depth += 1
+        body = self.visit(o.body, **kwargs)
+        self.depth -= 1
+        return self.join_lines(header, body, footer)
+
+    def visit_WhileLoop(self, o, **kwargs):
+        """
+        Format loop as
+          while (<condition>) {
+            ...body...
+          }
+        """
+        if o.condition is not None:
+            condition = self.visit(o.condition, **kwargs)
+        else:
+            condition = '1'
+        header = self.format_line('while (', condition, ') {')
+        footer = self.format_line('}')
+        self.depth += 1
+        body = self.visit(o.body, **kwargs)
+        self.depth -= 1
+        return self.join_lines(header, body, footer)
+
+    def visit_Conditional(self, o, **kwargs):
+        """
+        Format conditional as
+          if (<condition>) {
+            ...body...
+          } [ else { ]
+            [...body...]
+          [}]
+        """
+        condition = self.visit(o.conditions[0], **kwargs)
+        header = self.format_line('if (', condition, ') {')
+        else_header = None
+        if o.else_body:
+            else_header = self.format_line('} else {')
+        footer = '}'
+        self.depth += 1
+        bodies = self.visit_all(o.bodies, **kwargs)
+        else_body = self.visit(o.else_body, **kwargs)
+        self.depth -= 1
+        if len(bodies) > 1:
+            raise NotImplementedError('Multi-body conditionals not yet supported')
+        return self.join_lines(header, *bodies, else_header, else_body, footer)
+
+    def visit_Statement(self, o, **kwargs):
+        """
+        Format statement as
+          <target> = <expr> [<comment>]
+        """
+        target = self.visit(o.target, **kwargs)
+        expr = self.visit(o.expr, **kwargs)
+        comment = None
+        if o.comment:
+            comment = '  {}'.format(self.visit(o.comment, **kwargs))
+        return self.format_line(target, ' = ', expr, ';', comment=comment)
+
+    def visit_Section(self, o, **kwargs):
+        """
+        Format the section's body.
+        """
+        return self.visit(o.body, **kwargs)
+
+    def visit_CallStatement(self, o, **kwargs):
+        """
+        Format call statement as
+          <name>(<args>)
+        """
+        args = self.visit_all(o.arguments, **kwargs)
+        assert not o.kwarguments
+        return self.format_line(o.name, '(', self.join_items(args), ');')
 
     def visit_SymbolType(self, o, **kwargs):  # pylint: disable=unused-argument
         if o.dtype == DataType.DERIVED_TYPE:
@@ -283,59 +398,12 @@ class CCodegen(Visitor):
         return c_intrinsic_type(o)
 
     def visit_TypeDef(self, o, **kwargs):
-        self._depth += 1
+        header = self.format_line('struct ', o.name.lower(), ' {')
+        footer = self.format_line('};')
+        self.depth += 1
         decls = self.visit(o.declarations, **kwargs)
-        self._depth -= 1
-        return 'struct %s {\n%s\n} ;' % (o.name.lower(), decls)
-
-    def visit_Comment(self, o, **kwargs):  # pylint: disable=unused-argument
-        text = o._source.string if o.text is None else o.text
-        return self.indent + text.replace('!', '//')
-
-    def visit_CommentBlock(self, o, **kwargs):
-        comments = [self.visit(c, **kwargs) for c in o.comments]
-        return '\n'.join(comments)
-
-    def visit_Loop(self, o, **kwargs):
-        self._depth += 1
-        body = self.visit(o.body, **kwargs)
-        self._depth -= 1
-        increment = '++' if o.bounds.step is None else '+=%s' % self.visit(o.bounds.step, **kwargs)
-        lvar = self.visit(o.variable, **kwargs)
-        lower = self.visit(o.bounds.start, **kwargs)
-        upper = self.visit(o.bounds.stop, **kwargs)
-        criteria = '<=' if o.bounds.step is None or eval(str(o.bounds.step)) > 0 else '>='
-        header = 'for (%s=%s; %s%s%s; %s%s)' % (lvar, lower, lvar, criteria, upper, lvar, increment)
-        return self.indent + '%s {\n%s\n%s}\n' % (header, body, self.indent)
-
-    def visit_WhileLoop(self, o, **kwargs):
-        condition = self.visit(o.condition, **kwargs)
-        self._depth += 1
-        body = self.visit(o.body, **kwargs)
-        self._depth -= 1
-        header = 'while (%s)' % condition
-        return self.indent + '%s {\n%s\n%s}\n' % (header, body, self.indent)
-
-    def visit_Statement(self, o, **kwargs):
-        stmt = '%s = %s;' % (self.visit(o.target, **kwargs), self.visit(o.expr, **kwargs))
-        comment = '  %s' % self.visit(o.comment, **kwargs) if o.comment is not None else ''
-        return self.indent + stmt + comment
-
-    def visit_Conditional(self, o, **kwargs):
-        self._depth += 1
-        bodies = [self.visit(b, **kwargs) for b in o.bodies]
-        else_body = self.visit(o.else_body, **kwargs)
-        self._depth -= 1
-        if len(bodies) > 1:
-            raise NotImplementedError('Multi-body conditionals not yet supported')
-
-        cond = self.visit(o.conditions[0], **kwargs)
-        main_branch = 'if (%s)\n%s{\n%s\n' % (cond, self.indent, bodies[0])
-        else_branch = '%s} else {\n%s\n' % (self.indent, else_body) if o.else_body else ''
-        return self.indent + main_branch + else_branch + '%s}\n' % self.indent
-
-    def visit_Intrinsic(self, o, **kwargs):  # pylint: disable=unused-argument
-        return o.text
+        self.depth -= 1
+        return self.join_lines(header, decls, footer)
 
 
 def cgen(ir):
