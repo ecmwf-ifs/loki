fb644824383fa92d830c5247505df2a2c3210326
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2020-08-07 14:34:45 +0100

    Added a rudimentary install script which allows to automate the installation of Loki with dependencies

---
+++ install.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+source /usr/local/apps/module/init/bash
+
+# Parameters 
+LOKI_HOME=$(git rev-parse --show-toplevel)
+VENV_HOME=${1:-$LOKI_HOME/loki_env}
+ANT_VERSION=1.10.8
+JAVA_VERSION=11.0.1
+
+# Derived variables
+ANT_INSTALL_DIR=${LOKI_HOME}/externals/ant
+export ANT_HOME=${ANT_INSTALL_DIR}/apache-ant-${ANT_VERSION}
+export ANT_OPTS="-Dhttp.proxyHost=proxy.ecmwf.int -Dhttp.proxyPort=3333 -Dhttps.proxyHost=proxy.ecmwf.int -Dhttps.proxyPort=3333"
+
+CLAW_INSTALL_DIR=${LOKI_HOME}/externals/claw
+OFP_HOME=${VENV_HOME}/src/open-fortran-parser
+
+# Load modules
+module load python3/3.7.1-01
+module load java/${JAVA_VERSION}
+module load cmake/3.15.0
+
+#
+# Create Python virtualenv
+#
+
+python3 -m venv "${VENV_HOME}"
+source "${VENV_HOME}/bin/activate"
+
+#
+# Clone and install dev version of Loki
+#
+
+cd "${LOKI_HOME}"
+pip install numpy  # Needed during next step
+pip install -r requirements.txt
+pip install -e .  # Installs Loki dev copy in editable mode
+
+#
+# Install ant
+#
+
+# Cache NetRexx if it doesn't exist (Download fails from time to time)
+NETREXX_TEMP=${HOME}/.ant/tempcache/NetRexx.zip
+mkdir -p "${HOME}/.ant/tempcache"
+if [[ $(shasum -a 1 "${NETREXX_TEMP}" | cut -d ' ' -f1) != "1a47bf7b5d0055d4a94befc999c593d15b66c119" ]]
+then
+  wget -O "${NETREXX_TEMP}" ftp://ftp.software.ibm.com/software/awdtools/netrexx/NetRexx.zip
+fi
+
+# Download, unpack and install ant
+ANT_ARCHIVE=apache-ant-${ANT_VERSION}-bin.tar.gz
+mkdir -p "${ANT_INSTALL_DIR}"
+rm -rf "${ANT_HOME}" "${ANT_INSTALL_DIR}/${ANT_ARCHIVE}"
+cd "${ANT_INSTALL_DIR}"
+wget -O "${ANT_ARCHIVE}" http://mirror.ox.ac.uk/sites/rsync.apache.org/ant/binaries/${ANT_ARCHIVE}
+tar -xzf "${ANT_ARCHIVE}"
+"${ANT_HOME}/bin/ant" -f "${ANT_HOME}/fetch.xml" -Ddest=optional
+
+#
+# Install CLAW+OMNI
+#
+
+mkdir -p "${CLAW_INSTALL_DIR}"
+cd "${CLAW_INSTALL_DIR}"
+rm -rf claw-compiler
+git clone --recursive https://github.com/claw-project/claw-compiler.git claw-compiler
+cd claw-compiler
+cmake -DCMAKE_INSTALL_PREFIX="${CLAW_INSTALL_DIR}" .
+make
+make install
+
+#
+# Install OFP
+#
+
+# HACK: Force OFP version and install Java deps
+echo "VERSION = '0.5.3'" > "${OFP_HOME}/open_fortran_parser/_version.py"
+python3 -m open_fortran_parser --deps
+
+# Copy downloaded binaries to lib
+cd "${OFP_HOME}" 
+mkdir -p lib
+cp open_fortran_parser/*.jar lib
+
+# Rebuild OFP binaries to include custom changes
+"${ANT_HOME}/bin/ant"
+
+#
+# Create env activation script
+#
+
+echo "
+# This script activates Loki's virtual environment, loads additional modules and sets dependend paths.
+#
+# Run as 'source loki-activate'
+. ${VENV_HOME}/bin/activate
+_OLD_CLASSPATH=\"\${CLASSPATH}\"
+CLASSPATH=\"\${CLASSPATH}:${VENV_HOME}/src/open-fortran-parser/open_fortran_parser/*\"
+export CLASSPATH
+module load java/${JAVA_VERSION}
+export PATH=\"${CLAW_INSTALL_DIR}/bin:$PATH\"
+" > "${LOKI_HOME}/loki-activate"
+
+#
+# Finish
+#
+
+echo ""
+echo "+---------------------------------------------------------------+"
+echo "|                                                               |"
+echo "|  Successfully installed Loki with dependencies.               |"
+echo "|  Activate with:                                               |"
+echo "|                                                               |"
+echo "|     source loki-activate                                      |"
+echo "|                                                               |"
+echo "+---------------------------------------------------------------+"
