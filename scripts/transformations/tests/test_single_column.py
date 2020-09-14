4b1ae77bff1b7a4d80accb4e85f9a56e0399e3b6
Michael Lange <Michael.Lange@ecmwf.int>
2020-09-14 09:46:49 +0100

    Loki-transform: Add explicit tests of dimension hoisting from calls

--- scripts/transformations/tests/test_single_column.py
+++ scripts/transformations/tests/test_single_column.py
@@ -78,7 +78,7 @@ def test_extract_sca_iteration(frontend):
     c = 5.345
     DO jk = 2, nz
       DO jl = jstart, jend
-        t(jl, jk) = c * k
+        t(jl, jk) = c * jk
         q(jl, jk) = q(jl, jk-1)  + t(jl, jk) * c
       END DO
     END DO
@@ -97,10 +97,132 @@ def test_extract_sca_iteration(frontend):
     exp_loop_nest = """
   c = 5.345
   DO jk=2,nz
-    t(jk) = c*k
+    t(jk) = c*jk
     q(jk) = q(jk - 1) + t(jk)*c
   END DO
 """
     assert exp_loop_nest in source.to_fortran()
     # OMNI automatically lower-cases everything
+    assert "q(nz) = q(nz)*c" in source.to_fortran().lower()
+
+
+@pytest.mark.parametrize('frontend', [OFP, OMNI, FP])
+def test_extract_sca_nested_level_zero(frontend):
+    """
+    Test nested subroutine call outside the vertical loop.
+    """
+    source = SourceFile.from_source(source="""
+  SUBROUTINE compute_column(jstart, jend, nlon, nz, q, t)
+    INTEGER, INTENT(IN)   :: jstart, jend  ! Explicit iteration bounds of the kernel
+    INTEGER, INTENT(IN)   :: nlon, nz      ! Size of the horizontal and vertical
+    REAL, INTENT(INOUT)   :: t(nlon,nz)
+    REAL, INTENT(INOUT)   :: q(nlon,nz)
+    INTEGER :: jk
+    REAL :: c
+
+    c = 5.345
+    call compute_level_zero(jstart, jend, nlon, nz, c, q(:,:), t)
+
+    DO JL = JSTART, JEND
+      Q(JL, NZ) = Q(JL, NZ) * C
+    END DO
+  END SUBROUTINE compute_column
+""", frontend=frontend)
+
+    level_zero = SourceFile.from_source(source="""
+  SUBROUTINE compute_level_zero(jstart, jend, nlon, nz, c, q, t)
+    INTEGER, INTENT(IN)   :: jstart, jend
+    INTEGER, INTENT(IN)   :: nlon, nz
+    REAL, INTENT(IN) :: c
+    REAL, INTENT(INOUT)   :: t(nlon,nz)
+    REAL, INTENT(INOUT)   :: q(nlon,nz)
+    INTEGER :: jl, jk
+
+    DO jk = 2, nz
+      DO jl = jstart, jend
+        t(jl, jk) = c * jk
+        q(jl, jk) = q(jl, jk-1)  + t(jl, jk) * c
+      END DO
+    END DO
+  END SUBROUTINE compute_level_zero
+""", frontend=frontend)
+
+    source['compute_column'].enrich_calls(routines=level_zero.all_subroutines)
+
+    # Apply single-column extraction trasnformation in topological order
+    dimension = Dimension(name='nlon', variable='jl', iteration=('jstart', 'jend'), aliases=[])
+    sca_transform = ExtractSCATransformation(dimension=dimension)
+    source.apply(sca_transform, role='kernel', targets=['compute_utility'])
+    level_zero.apply(sca_transform, role='kernel')
+
+    exp_loop_nest = """
+  DO jk=2,nz
+    t(jk) = c*jk
+    q(jk) = q(jk - 1) + t(jk)*c
+  END DO
+"""
+    assert exp_loop_nest in level_zero.to_fortran()
+
+    # OMNI automatically lower-cases everything
+    assert "call compute_level_zero(nz, c, q(:), t(:))" in source.to_fortran().lower()
+    assert "q(nz) = q(nz)*c" in source.to_fortran().lower()
+
+
+@pytest.mark.parametrize('frontend', [OFP, OMNI, FP])
+def test_extract_sca_nested_level_one(frontend):
+    """
+    Test nested subroutine call inside vertical loop.
+    """
+    source = SourceFile.from_source(source="""
+  SUBROUTINE compute_column(jstart, jend, nlon, nz, q, t)
+    INTEGER, INTENT(IN)   :: jstart, jend  ! Explicit iteration bounds of the kernel
+    INTEGER, INTENT(IN)   :: nlon, nz      ! Size of the horizontal and vertical
+    REAL, INTENT(INOUT)   :: t(nlon,nz)
+    REAL, INTENT(INOUT)   :: q(nlon,nz)
+    INTEGER :: jk
+    REAL :: c
+
+    c = 5.345
+    DO jk = 2, nz
+      call compute_level_one(jstart, jend, nlon, jk, c, q(:,jk), t(:,jk))
+    END DO
+
+    DO JL = JSTART, JEND
+      Q(JL, NZ) = Q(JL, NZ) * C
+    END DO
+  END SUBROUTINE compute_column
+""", frontend=frontend)
+
+    level_one = SourceFile.from_source(source="""
+  SUBROUTINE compute_level_one(jstart, jend, nlon, jk, c, q, t)
+    INTEGER, INTENT(IN)   :: jstart, jend
+    INTEGER, INTENT(IN)   :: nlon, jk
+    REAL, INTENT(IN) :: c
+    REAL, INTENT(INOUT)   :: t(nlon,nz)
+    REAL, INTENT(INOUT)   :: q(nlon,nz)
+    INTEGER :: jl, jk
+
+    DO jl = jstart, jend
+      t(jl, jk) = c * jk
+      q(jl, jk) = q(jl, jk-1)  + t(jl, jk) * c
+    END DO
+  END SUBROUTINE compute_level_one
+""", frontend=frontend)
+
+    source['compute_column'].enrich_calls(routines=level_one.all_subroutines)
+
+    # Apply single-column extraction trasnformation in topological order
+    dimension = Dimension(name='nlon', variable='jl', iteration=('jstart', 'jend'), aliases=[])
+    sca_transform = ExtractSCATransformation(dimension=dimension)
+    source.apply(sca_transform, role='kernel', targets=['compute_utility'])
+    level_one.apply(sca_transform, role='kernel')
+
+    exp_loop_nest = """
+  t(jk) = c*jk
+  q(jk) = q(jk - 1) + t(jk)*c
+"""
+    assert exp_loop_nest in level_one.to_fortran()
+
+    # OMNI automatically lower-cases everything
+    assert "call compute_level_one(jk, c, q(jk), t(jk))" in source.to_fortran().lower()
     assert "q(nz) = q(nz)*c" in source.to_fortran().lower()
