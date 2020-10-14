d6347ca70c278226c4837d19528581f3a5679b01
Michael Lange <Michael.Lange@ecmwf.int>
2020-10-14 15:28:57 +0100

    Transform: Rename dependency.py -> dependency_transform.py

--- loki/transform/__init__.py
+++ loki/transform/__init__.py
@@ -1,7 +1,7 @@
 from loki.transform.transformation import * # noqa
 from loki.transform.transform_utilities import * # noqa
 from loki.transform.transform_array_indexing import * # noqa
-from loki.transform.dependency import * # noqa
+from loki.transform.dependency_transform import * # noqa
 from loki.transform.fortran_c_transform import * # noqa
 from loki.transform.fortran_max_transform import * # noqa
 from loki.transform.fortran_python_transform import * # noqa
