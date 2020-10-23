3604c846bbe691c86c0c45e1d58781220f1d9ddc
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2020-10-23 17:30:52 +0100

    Mechanics for loop fusion with pragma annotations and explicit loop range injection

--- loki/transform/transform_loop.py
+++ loki/transform/transform_loop.py
@@ -2,26 +2,96 @@
 Collection of utility routines that provide loop transformations.
 
 """
-from loki.ir import Loop
-from loki.tools import is_loki_pragma, flatten
+from collections import defaultdict
+
+from loki.expression import symbols as sym, SubstituteExpressions
+from loki.ir import Loop, Conditional, Comment
+from loki.logging import info
+from loki.tools import is_loki_pragma, get_pragma_parameters, flatten, as_tuple
 from loki.visitors import FindNodes, Transformer
 
-__all__ = ['fuse_loops']
+__all__ = ['loop_fusion']
 
 
-def fuse_loops(routine):
+def loop_fusion(routine):
     """
-    Searches for loops annotated with the `loki fuse` pragma and attempts
+    Search for loops annotated with the `loki loop-fusion` pragma and attempt
     to fuse them into a single loop.
     """
-    loop_list = [loop for loop in FindNodes(Loop).visit(routine.body)
-                 if is_loki_pragma(loop.pragma, starts_with='fuse')]
-    if not loop_list:
+    # Extract all annotated loops and sort them into fusion groups
+    fusion_groups = defaultdict(list)
+    fusion_variables = {}
+    fusion_ranges = {}
+    for loop in FindNodes(Loop).visit(routine.body):
+        if is_loki_pragma(loop.pragma, starts_with='loop-fusion'):
+            parameters = get_pragma_parameters(loop.pragma, starts_with='loop-fusion')
+            group = parameters.get('group', 'default')
+            fusion_groups[group] += [loop]
+            fusion_variables.setdefault(group, loop.variable)
+
+            if 'range' in parameters:
+                bounds = []
+                for bound in parameters['range'].split(':'):
+                    if bound.isnumeric():
+                        bounds += [sym.IntLiteral(bound)]
+                    # TODO: parse more complex expressions
+                    else:
+                        bounds += [sym.Variable(name=bound, scope=routine.symbols)]
+                loop_range = sym.LoopRange(as_tuple(bounds))
+            else:
+                loop_range = loop.bounds
+            fusion_ranges.setdefault(group, loop_range)
+            # TODO: Check step (None vs 1)
+            if fusion_ranges[group].start != loop_range.start or fusion_ranges[group].stop != loop_range.stop:
+                raise RuntimeError('Loop ranges in group "{}" do not match'.format(group))
+
+    if not fusion_groups:
         return
 
-    variable = loop_list[0].variable
-    bounds = loop_list[0].bounds
-    body = flatten(loop.body for loop in loop_list)
-    loop_map = {loop_list[0]: Loop(variable=variable, body=body, bounds=bounds)}
-    loop_map.update({loop: None for loop in loop_list[1:]})
+    missing_ranges = set(fusion_groups.keys()) - set(fusion_ranges.keys())
+    if missing_ranges:
+        raise RuntimeError('No loop ranges given for group(s) "{}".'.format('", "'.join(missing_ranges)))
+
+    # Built merged loops
+    loop_map = {}
+    for group, loop_list in fusion_groups.items():
+        loop_range = fusion_ranges[group]
+        loop_variable = fusion_variables[group]
+
+        bodies = []
+        for loop in loop_list:
+            # Replace loop variable if it differs
+            if loop.variable != loop_variable:
+                body = SubstituteExpressions({loop.variable: loop_variable}).visit(loop.body)
+            else:
+                body = loop.body
+
+            # Enclose in a conditional block if loop range is smaller
+            conditions = []
+            if loop.bounds.start != loop_range.start:
+                conditions += [sym.Comparison(loop_variable, '>=', loop.bounds.start)]
+            if loop.bounds.stop != loop_range.stop:
+                conditions += [sym.Comparison(loop_variable, '<=', loop.bounds.stop)]
+            if loop.bounds.step not in (None, loop_range.step or 1):
+                shifted_variable = sym.Sum(loop_variable, sym.Product(-1, loop.bounds.start))
+                call = sym.InlineCall('mod', parameters=(shifted_variable, loop.bounds.step))
+                conditions += [sym.Comparison(call, '==', sym.Literal(0))]
+            if conditions:
+                if len(conditions) == 1:
+                    condition = conditions[0]
+                else:
+                    condition = sym.LogicalAnd(conditions)
+                bodies += [Conditional(conditions=[condition], bodies=[body], else_body=())]
+            else:
+                bodies += [body]
+
+        loop_map[loop_list[0]] = (
+            Comment('! Loki transformation loop-fusion group({})'.format(group)),
+            Loop(variable=loop_variable, body=flatten(bodies), bounds=loop_range))
+        loop_map.update({loop: None for loop in loop_list[1:]})
+
+    # Apply transformation
     routine.body = Transformer(loop_map).visit(routine.body)
+
+    info('%s: fused %d loops in %d groups.', routine.name,
+         sum(len(loop_list) for loop_list in fusion_groups.values()), len(fusion_groups))
--- tests/test_transform_loop.py
+++ tests/test_transform_loop.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from conftest import jit_compile, clean_test
 from loki import Subroutine, OFP, OMNI, FP, FindNodes, Loop
-from loki.transform import fuse_loops
+from loki.transform import loop_fusion
 
 
 @pytest.fixture(scope='module', name='here')
@@ -19,17 +19,16 @@ def test_transform_loop_fuse_matching(here, frontend):
     """
     fcode = """
 subroutine transform_loop_fuse_matching(a, b, n)
-  use iso_fortran_env, only: real64
-  real(kind=real64), intent(out) :: a(n), b(n)
+  integer, intent(out) :: a(n), b(n)
   integer, intent(in) :: n
   integer :: i
 
-  !$loki fuse
+  !$loki loop-fusion
   do i=1,n
     a(i) = i
   end do
 
-  !$loki fuse
+  !$loki loop-fusion
   do i=1,n
     b(i) = n-i+1
   end do
@@ -41,26 +40,189 @@ end subroutine transform_loop_fuse_matching
 
     # Test the reference solution
     n = 100
-    a = np.zeros(shape=(n,))
-    b = np.zeros(shape=(n,))
+    a = np.zeros(shape=(n,), dtype=np.int32)
+    b = np.zeros(shape=(n,), dtype=np.int32)
     function(a=a, b=b, n=n)
     assert np.all(a == range(1, n+1))
     assert np.all(b == range(n, 0, -1))
 
     # Apply transformation
     assert len(FindNodes(Loop).visit(routine.body)) == 2
-    fuse_loops(routine)
+    loop_fusion(routine)
     assert len(FindNodes(Loop).visit(routine.body)) == 1
 
     fused_filepath = here/('%s_fused_%s.f90' % (routine.name, frontend))
     fused_function = jit_compile(routine, filepath=fused_filepath, objname=routine.name)
 
     # Test transformation
-    a = np.zeros(shape=(n,))
-    b = np.zeros(shape=(n,))
+    a = np.zeros(shape=(n,), dtype=np.int32)
+    b = np.zeros(shape=(n,), dtype=np.int32)
     fused_function(a=a, b=b, n=n)
     assert np.all(a == range(1, n+1))
     assert np.all(b == range(n, 0, -1))
 
     clean_test(filepath)
     clean_test(fused_filepath)
+
+
+@pytest.mark.parametrize('frontend', [OFP, OMNI, FP])
+def test_transform_loop_fuse_subranges(here, frontend):
+    """
+    Apply loop fusion with annotated range for loops with
+    non-matching iteration spaces.
+    """
+    fcode = """
+subroutine transform_loop_fuse_subranges(a, b, n)
+  integer, intent(out) :: a(n), b(n)
+  integer, intent(in) :: n
+  integer :: i, j
+
+  a(:) = 0
+  b(:) = 0
+
+  !$loki loop-fusion
+  do i=1,n
+    a(i) = a(i) + i
+  end do
+
+  !$loki loop-fusion range(1:n)
+  do j=1,15
+    b(j) = b(j) + n-j+1
+  end do
+
+  !$loki loop-fusion range(1:n)
+  do i=16,n
+    b(i) = b(i) + n-i+1
+  end do
+end subroutine transform_loop_fuse_subranges
+"""
+    routine = Subroutine.from_source(fcode, frontend=frontend)
+    filepath = here/('%s_%s.f90' % (routine.name, frontend))
+    function = jit_compile(routine, filepath=filepath, objname=routine.name)
+
+    # Test the reference solution
+    n = 100
+    a = np.zeros(shape=(n,), dtype=np.int32)
+    b = np.zeros(shape=(n,), dtype=np.int32)
+    function(a=a, b=b, n=n)
+    assert np.all(a == range(1, n+1))
+    assert np.all(b == range(n, 0, -1))
+
+    # Apply transformation
+    assert len(FindNodes(Loop).visit(routine.body)) == 3
+    loop_fusion(routine)
+    assert len(FindNodes(Loop).visit(routine.body)) == 1
+
+    fused_filepath = here/('%s_fused_%s.f90' % (routine.name, frontend))
+    fused_function = jit_compile(routine, filepath=fused_filepath, objname=routine.name)
+
+    # Test transformation
+    a = np.zeros(shape=(n,), dtype=np.int32)
+    b = np.zeros(shape=(n,), dtype=np.int32)
+    fused_function(a=a, b=b, n=n)
+    assert np.all(a == range(1, n+1))
+    assert np.all(b == range(n, 0, -1))
+
+    clean_test(filepath)
+    clean_test(fused_filepath)
+
+
+@pytest.mark.parametrize('frontend', [OFP, OMNI, FP])
+def test_transform_loop_fuse_groups(here, frontend):
+    """
+    Apply loop fusion for multiple loop fusion groups.
+    """
+    fcode = """
+subroutine transform_loop_fuse_groups(a, b, c, n)
+  integer, intent(out) :: a(n), b(n), c(n)
+  integer, intent(in) :: n
+  integer :: i
+
+  c(1) = 1
+
+  !$loki loop-fusion group(g1)
+  do i=1,n
+    a(i) = i
+  end do
+
+  !$loki loop-fusion group(g1)
+  do i=1,n
+    b(i) = n-i+1
+  end do
+
+  !$loki loop-fusion group(loop-group2)
+  do i=1,n
+    a(i) = a(i) + 1
+  end do
+
+  !$loki loop-fusion group(loop-group2)
+  do i=1,n
+    b(i) = b(i) + 1
+  end do
+
+  !$loki loop-fusion group(g1) range(1:n)
+  do i=2,n
+    c(i) = c(i-1) + 1
+  end do
+end subroutine transform_loop_fuse_groups
+"""
+    routine = Subroutine.from_source(fcode, frontend=frontend)
+    filepath = here/('%s_%s.f90' % (routine.name, frontend))
+    function = jit_compile(routine, filepath=filepath, objname=routine.name)
+
+    # Test the reference solution
+    n = 100
+    a = np.zeros(shape=(n,), dtype=np.int32)
+    b = np.zeros(shape=(n,), dtype=np.int32)
+    c = np.zeros(shape=(n,), dtype=np.int32)
+    function(a=a, b=b, c=c, n=n)
+    assert np.all(a == range(2, n+2))
+    assert np.all(b == range(n+1, 1, -1))
+    assert np.all(c == range(1, n+1))
+
+    # Apply transformation
+    assert len(FindNodes(Loop).visit(routine.body)) == 5
+    loop_fusion(routine)
+    assert len(FindNodes(Loop).visit(routine.body)) == 2
+
+    fused_filepath = here/('%s_fused_%s.f90' % (routine.name, frontend))
+    fused_function = jit_compile(routine, filepath=fused_filepath, objname=routine.name)
+
+    # Test transformation
+    a = np.zeros(shape=(n,), dtype=np.int32)
+    b = np.zeros(shape=(n,), dtype=np.int32)
+    c = np.zeros(shape=(n,), dtype=np.int32)
+    fused_function(a=a, b=b, c=c, n=n)
+    assert np.all(a == range(2, n+2))
+    assert np.all(b == range(n+1, 1, -1))
+    assert np.all(c == range(1, n+1))
+
+    clean_test(filepath)
+    clean_test(fused_filepath)
+
+
+@pytest.mark.parametrize('frontend', [OFP, OMNI, FP])
+def test_transform_loop_fuse_failures(here, frontend):
+    """
+    Test that loop-fusion fails for known mistakes.
+    """
+    fcode = """
+subroutine transform_loop_fuse_non_matching(a, b, n)
+  integer, intent(out) :: a(n), b(n)
+  integer, intent(in) :: n
+  integer :: i
+
+  !$loki loop-fusion group(1)
+  do i=1,n
+    a(i) = i
+  end do
+
+  !$loki loop-fusion group(1)
+  do i=0,n-1
+    b(i+1) = n-i
+  end do
+end subroutine transform_loop_fuse_non_matching
+"""
+    routine = Subroutine.from_source(fcode, frontend=frontend)
+    with pytest.raises(RuntimeError):
+        loop_fusion(routine)
