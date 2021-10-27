8fb4cdd3793e6ac99d3e6c5b5ccdc35af2e0b124
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2021-10-27 15:30:39 +0100

    Update setup procedure to use pyproject.toml and setup.cfg for declaring requirements and options

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-
 import os
-import setuptools.ssl_support
+import setuptools
 
 ca_bundle = (os.environ.get('SSL_CERT_FILE') or
              os.environ.get('REQUESTS_CA_BUNDLE') or
@@ -12,13 +7,4 @@ ca_bundle = (os.environ.get('SSL_CERT_FILE') or
 if ca_bundle:
     setuptools.ssl_support.cert_paths = [ca_bundle]
 
-setup(name='loki',
-      use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      description="Experimental Fortran IR to facilitate source-to-source transformations",
-      author="Michael Lange",
-      packages=find_packages(exclude=['tests']),
-      install_requires=['open-fortran-parser'],
-      test_requires=['pytest', 'flake8'],
-      scripts=['scripts/loki-transform.py', 'scripts/loki-lint.py'],
-     )
+setuptools.setup()
