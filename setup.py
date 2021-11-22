34d2a85281028d0cd7d0db7d75632dea3421d52b
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2021-11-22 17:48:07 +0000

    Disable ssl certificate path setter for setuptools>=57.1

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ ca_bundle = (os.environ.get('SSL_CERT_FILE') or
              os.environ.get('REQUESTS_CA_BUNDLE') or
              os.environ.get('CURL_CA_BUNDLE'))
 if ca_bundle:
-    setuptools.ssl_support.cert_paths = [ca_bundle]
+    # Provide custom CA paths to setuptools when behind a proxy server that
+    # uses a self-signed certificate for SSL connections.
+    # Note: setuptools>=57.1.0 dropped ssl_support and relies entirely on urllib,
+    #       thus proxy certificates passed to pip should already be used.
+    from pkg_resources import parse_version  # pylint: disable=import-outside-toplevel
+    if parse_version(setuptools.__version__) < parse_version("57.1.0"):
+        import setuptools.ssl_support  # pylint: disable=import-outside-toplevel,ungrouped-imports
+        setuptools.ssl_support.cert_paths = [ca_bundle]
 
 setuptools.setup()
