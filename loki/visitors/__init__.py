8cb63228e23d42869c5e6226a39e2ef3c1ea5bfd
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2021-12-14 20:37:30 +0000

    Rename loki.visitors.print to loki.visitors.pprint to avoid overwriting built-in "print" when using "from loki import *"

--- loki/visitors/__init__.py
+++ loki/visitors/__init__.py
@@ -1,4 +1,4 @@
 from loki.visitors.visitor import *  # noqa
 from loki.visitors.transform import *  # noqa
 from loki.visitors.find import *  # noqa
-from loki.visitors.print import *  # noqa
+from loki.visitors.pprint import *  # noqa
