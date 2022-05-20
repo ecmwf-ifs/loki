bcdd39bc9f2dc1ba2815a2d223fc57f0ddb40427
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-05-20 09:27:06 +0000

    Make ProcedureType more resilient when weakref is dead

--- loki/types.py
+++ loki/types.py
@@ -137,58 +137,98 @@ class ProcedureType(DataType):
     """
     Representation of a function or subroutine type definition.
 
+    This serves also as the cross-link between the use of a procedure (e.g. in a
+    :any:`CallStatement`) to the :any:`Subroutine` object that is the target of
+    a call. If the corresponding object is not yet available when the
+    :any:`ProcedureType` object is created, or its definition is transient and
+    subject to IR rebuilds (e.g. :any:`StatementFunction`), the :any:`LazyNodeLookup`
+    utility can be used to defer the actual instantiation. In that situation,
+    :data:`name` should be provided in addition.
+
     Parameters
     ----------
     name : str, optional
         The name of the function or subroutine. Can be skipped if :arg:`procedure`
-        is provided
+        is provided (not in the form of a :any:`LazyNodeLookup`)
     is_function : bool, optional
         Indicate that this is a function
     is_generic : bool, optional
         Indicate that this is a generic function
-    procedure : :any:`Subroutine` or :any:`StatementFunction`
+    procedure : :any:`Subroutine` or :any:`StatementFunction` or :any:`LazyNodeLookup`, optional
         The procedure this type represents
     """
 
     def __init__(self, name=None, is_function=False, is_generic=False, procedure=None, return_type=None):
+        from loki.subroutine import Subroutine  # pylint: disable=import-outside-toplevel
         super().__init__()
-        assert name or procedure
+        assert name or isinstance(procedure, Subroutine)
         assert isinstance(return_type, SymbolAttributes) or procedure or not is_function
-        self._name = name
-        self._is_function = is_function
         self.is_generic = is_generic
-        self._return_type = return_type
-        if procedure is None:
-            self._procedure = None
-        elif isinstance(procedure, LazyNodeLookup):
+        if procedure is None or isinstance(procedure, LazyNodeLookup):
             self._procedure = procedure
+            self._name = name
+            self._is_function = is_function
+            self._return_type = return_type
+            # NB: not applying an assert on the procedure name for LazyNodeLookup as
+            # the point of the lazy lookup is that we might not have the the procedure
+            # definition available at type instantiation time
         else:
             self._procedure = weakref.ref(procedure)
+            # Cache all properties for when procedure link becomes inactive
+            assert name is None or name.lower() == self.procedure.name.lower()
+            self._name = self.procedure.name
+            assert is_function is None or is_function == self.procedure.is_function
+            self._is_function = self.procedure.is_function
+            # TODO: compare return type once type comparison is more robust
+            self._return_type = self.procedure.return_type
 
     @property
     def name(self):
+        """
+        The name of the procedure
+
+        This looks up the name in the linked :attr:`procedure` if available, otherwise
+        returns the name stored during instanation of the :any:`ProcedureType` object.
+        """
         return self._name if self.procedure is BasicType.DEFERRED else self.procedure.name
 
     @property
     def procedure(self):
+        """
+        The :any:`Subroutine` object of the procedure
+
+        If not provided during instantiation or if the underlying :any:`weakref` is dead,
+        this returns :any:`BasicType.DEFERRED`.
+        """
         if self._procedure is None:
+            return BasicType.DEFERRED
+        if self._procedure() is None:
             return BasicType.DEFERRED
         return self._procedure()
 
     @property
     def parameters(self):
+        """
+        The tuple of procedure arguments, if :attr:`procedure` is available
+        """
         if self.procedure is BasicType.DEFERRED:
             return tuple()
         return self.procedure.arguments
 
     @property
     def is_function(self):
+        """
+        Return `True` if the procedure is a function, otherwise `False`
+        """
         if self.procedure is BasicType.DEFERRED:
             return self._is_function
         return self.procedure.is_function
 
     @property
     def return_type(self):
+        """
+        The return type of the function (or `None`)
+        """
         if self.procedure is BasicType.DEFERRED:
             return self._return_type
         return self.procedure.return_type
