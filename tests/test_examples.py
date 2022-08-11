4607f358f0402cdbd0dbc7e413d6894bff1b7ac5
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-08-11 15:45:50 +0100

    Skip example tests if IPython or jupyter not available

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,9 +5,28 @@ from pathlib import Path
 
 import pytest
 import nbformat
+from jupyter_client.kernelspec import find_kernel_specs
 from nbconvert.preprocessors import ExecutePreprocessor
 
 example_path = Path(__file__).parent.parent/'example'
+
+def is_ipython_available():
+    """
+    Check if Jupyter Notebook kernel is available
+    """
+    is_available = False
+    try:
+        import IPython  # pylint: disable=import-outside-toplevel,unused-import
+        is_available = True
+    except ImportError:
+        pass
+    return is_available
+
+# Skip tests in this module if Jupyter Kernel not available
+pytestmark = pytest.mark.skipif(
+    not is_ipython_available() or 'python3' not in find_kernel_specs(),
+    reason='IPython or Jupyter kernel are not available'
+)
 
 @pytest.mark.parametrize("notebook", example_path.glob('*.ipynb'))
 def test_notebooks(notebook, monkeypatch):
@@ -19,5 +38,5 @@ def test_notebooks(notebook, monkeypatch):
 
     with notebook.open() as f:
         nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=600,kernel_name='python3')
+        ep = ExecutePreprocessor(timeout=60, kernel_name='python3')
         assert ep.preprocess(nb) is not None, f"Got empty notebook for {notebook}"
