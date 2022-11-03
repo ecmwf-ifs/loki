c29dca251d8e040e72af73ddce1b96c7ccb4663f
Michael Lange <Michael.Lange@ecmwf.int>
2022-11-03 15:21:53 +0000

    ResolveAssociations: Re-write as Transformer, called from a routine
    We can now apply this transformation on arbitrary code block, although
    we need to trigger a rescope, if we apply it to subroutines.

--- loki/transform/transform_associates.py
+++ loki/transform/transform_associates.py
@@ -4,7 +4,7 @@ from loki.tools import CaseInsensitiveDict
 from loki.visitors import Transformer, FindNodes
 
 
-__all__ = ['resolve_associates']
+__all__ = ['resolve_associates', 'ResolveAssociatesTransformer']
 
 
 def resolve_associates(routine):
@@ -16,18 +16,34 @@ def resolve_associates(routine):
     routine : :any:`Subroutine`
         The subroutine for which to resolve all associate blocks.
     """
-    assoc_map = {}
-    vmap = {}
-    for assoc in FindNodes(Associate).visit(routine.body):
-        invert_assoc = CaseInsensitiveDict({v.name: k for k, v in assoc.associations})
-        for v in FindVariables(unique=False).visit(assoc.body):
-            if v.name in invert_assoc:
-                inv = invert_assoc[v.name]
-                vmap[v] = v.clone(parent=inv.parent, scope=inv.scope)
-        assoc_map[assoc] = assoc.body
+    routine.body = ResolveAssociatesTransformer().visit(routine.body)
 
-    routine.body = Transformer(assoc_map).visit(routine.body)
-    routine.body = SubstituteExpressions(vmap).visit(routine.body)
-
-    # Ensure that all symbols have the appropriate scope attached
+    # Ensure that all symbols have the appropriate scope attached.
+    # This is needed, as the parent of a symbol might have changed,
+    # which affects the symbol's type-defining scope.
     routine.rescope_symbols()
+
+
+class ResolveAssociatesTransformer(Transformer):
+    """
+    :any:`Transformer` class that replaces :any:`Associate` node with its body,
+    with  have been replaced by their ass
+    """
+
+    def visit_Associate(self, o):
+        # First head-recurse, so that all associate blocks beneath are resolved
+        body = self.visit(o.body)
+
+        # Create an inverse association map to look up replacements
+        invert_assoc = CaseInsensitiveDict({v.name: k for k, v in o.associations})
+
+        # Build the expression substitution map
+        vmap = {}
+        for v in FindVariables(unique=False).visit(body):
+            if v.name in invert_assoc:
+                # Clone the expression to update its parentage and scoping
+                inv = invert_assoc[v.name]
+                vmap[v] = v.clone(name=inv.name, parent=inv.parent, scope=inv.scope)
+
+        # Return the body of the associate block with all expressions replaced
+        return SubstituteExpressions(vmap).visit(body)
