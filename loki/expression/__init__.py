8f130adb036091697a7e51235cb8098d81ffed9e
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-11-15 09:02:10 +0000

    Rename loki.expression.expression -> loki.expression.expr_visitors

--- loki/expression/__init__.py
+++ loki/expression/__init__.py
@@ -1,4 +1,4 @@
-from loki.expression.expression import *  # noqa
+from loki.expression.expr_visitors import *  # noqa
 from loki.expression.symbols import *  # noqa
 from loki.expression.operations import *  # noqa
 from loki.expression.mappers import *  # noqa
