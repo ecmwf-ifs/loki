0abd6382ca13f2488bb791ae0d63c981bb90ca96
Michael Lange <Michael.Lange@ecmwf.int>
2022-11-15 08:30:04 +0000

    ArgumentShape: Use CaseInsensitiveDict and dro string conversion

--- scripts/transformations/transformations/argument_shape.py
+++ scripts/transformations/transformations/argument_shape.py
@@ -16,7 +16,8 @@ declarations and call signatures.
 
 from loki import (
     Transformation, FindNodes, CallStatement, Array, FindVariables,
-    SubstituteExpressions, BasicType, as_tuple, Transformer
+    SubstituteExpressions, BasicType, as_tuple, Transformer,
+    CaseInsensitiveDict
 )
 
 
@@ -60,25 +61,25 @@ class ArgumentArrayShapeAnalysis(Transformation):
             for arg, val in call.arg_iter():
                 if isinstance(arg, Array) and len(arg.shape) > 0:
                     # Only create new shapes for deferred dimension args
-                    if all(str(d) == ':' for d in arg.shape):
+                    if all(d == ':' for d in arg.shape):
                         if len(val.shape) == len(arg.shape):
                             # We're passing the full value array, copy shape
                             vmap[arg] = arg.clone(type=arg.type.clone(shape=val.shape))
                         else:
                             # Passing a sub-array of val, find the right index
                             new_shape = [s for s, d in zip(val.shape, val.dimensions)
-                                         if str(d) == ':']
+                                         if d == ':']
                             vmap[arg] = arg.clone(type=arg.type.clone(shape=new_shape))
 
             # Propagate the updated variables to variable definitions in routine
             routine.variables = [vmap.get(v, v) for v in routine.variables]
 
             # And finally propagate this to the variable instances
-            vname_map = {k.name.lower(): v for k, v in vmap.items()}
+            vname_map = CaseInsensitiveDict((k.name, v) for k, v in vmap.items())
             vmap_body = {}
             for v in FindVariables(unique=False).visit(routine.body):
-                if v.name.lower() in vname_map:
-                    new_shape = vname_map[v.name.lower()].shape
+                if v.name in vname_map:
+                    new_shape = vname_map[v.name].shape
                     vmap_body[v] = v.clone(type=v.type.clone(shape=new_shape))
             routine.body = SubstituteExpressions(vmap_body).visit(routine.body)
 
