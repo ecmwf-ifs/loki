f873a3f5be5a2b51551dd965d871d8590d883302
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2023-03-07 17:41:20 +0100

    WIP: Offload transformation now runs in single-pass, still not tested

--- loki/transform/transform_global_var_offload.py
+++ loki/transform/transform_global_var_offload.py
@@ -1,71 +1,56 @@
 from loki.transform.transformation import Transformation
 from loki.bulk.item import GlobalVarImportItem
 from loki.analyse import dataflow_analysis_attached
-from loki.tools.util import as_tuple
-from loki.ir import VariableDeclaration, Pragma
+from loki.ir import Pragma, CallStatement, Import
 from loki.visitors.find import FindNodes
 from loki.visitors.transform import Transformer
+from loki.expression.expr_visitors import FindInlineCalls
+import re
 
-__all__ = ['GlobalVarImportAnalysis', 'GlobalVarOffloadTransformation']
-
-class GlobalVarImportAnalysis(Transformation):
-
-    def transform_subroutine(self, routine, **kwargs):
-        with dataflow_analysis_attached(routine):
-            for item in [s for s in kwargs.get('successors', None) if isinstance(s, GlobalVarImportItem)]:
-                _symbol = item.name.split('#')[-1]
-                if _symbol in routine.body.uses_symbols:
-                    item.trafo_data.update({'acc_copyin': as_tuple(_symbol.lower())})
-                if _symbol in routine.body.defines_symbols:
-                    item.trafo_data.update({'acc_copyout': as_tuple(_symbol.lower())})
+__all__ = ['GlobalVarOffloadTransformation']
 
 class GlobalVarOffloadTransformation(Transformation):
 
     def __init__(self):
+
         self._acc_copyin = set()
         self._acc_copyout = set()
+#        self._imports = set()
 
     def transform_module(self, module, **kwargs):
+
         item = kwargs['item']
 
-        copyin = item.trafo_data.get('acc_copyin')
-        copyout = item.trafo_data.get('acc_copyout')
-
-        #... skip transformation if symbol was imported but never used
-        if not copyin and not copyout:
+        #... bail if not global variable import
+        if not isinstance(item, GlobalVarImportItem):
             return
 
-        #... confirm that any vars to be offloaded are declared in module
+        #... confirm that var to be offloaded is declared in module
+        _symbol = item.name.split('#')[-1].lower()
         decl_symbs = [s.name.lower() for s in module.variables]
-        for var in [v for v in [copyin, copyout] if v]:
-            assert var[0] in decl_symbs
+        assert _symbol in decl_symbs
 
         sym_dict = {s.name.lower(): s for s in module.variables}
         pragmas = [p for p in FindNodes(Pragma).visit(module.spec) if p.keyword.lower() == 'acc']
 
-        for var in [v for v in [copyin, copyout] if v]:
+        #... do nothing if var is a parameter
+        if sym_dict[_symbol].type.parameter:
+            return
 
-            #... do nothing if var is a parameter
-            if sym_dict[var[0]].type.parameter:
-                continue
+        #... check if var is already declared
+        for p in pragmas:
+            if re.search(fr'\b{_symbol}\b', p.content.lower()):
+                return
 
-            #... check if var is already declared
-            for p in pragmas:
-                if var[0] in p.content.lower():
-                    continue
-
-            module.spec.append(Pragma(keyword='acc', content=f'declare create({var[0].lower()})'))
-            if copyin:
-                if var[0] in copyin:
-                    self._acc_copyin.add(var[0])
-            if copyout:
-                if var[0] in copyout:
-                    self._acc_copyout.add(var[0])
+        module.spec.append(Pragma(keyword='acc', content=f'declare create({_symbol})'))
 
     def transform_subroutine(self, routine, **kwargs):
+
         role = kwargs.get('role')
         if role == 'driver':
             self.process_driver(routine, **kwargs)
+        elif role == 'kernel':
+            self.process_kernel(routine, **kwargs)
 
     def process_driver(self, routine, **kwargs):
 
@@ -77,3 +62,22 @@ class GlobalVarOffloadTransformation(Transformation):
                 pragma_map.update({pragma: Pragma(keyword='acc', content='update self(' + ','.join(self._acc_copyout) + ')')})
 
         routine.body = Transformer(pragma_map).visit(routine.body)
+
+    def process_kernel(self, routine, **kwargs):
+
+        _imports = FindNodes(Import).visit(routine.spec)
+        _calls = [c.name for c in FindNodes(CallStatement).visit(routine.body)]
+        _icalls = FindInlineCalls().visit(routine.body)
+
+        _imports = [i for i in _imports if i.symbols not in _calls]
+        _imports = [i for i in _imports if i.symbols not in _icalls]
+
+        _import_dict = {s.name.lower(): i.module for i in _imports for s in i.symbols}
+        _imports = [s.name.lower() for i in _imports for s in i.symbols]
+
+        with dataflow_analysis_attached(routine):
+            for item in [i for i in _imports if i in kwargs.get('targets')]:
+                if item in routine.body.uses_symbols:
+                    self._acc_copyin.add(item)
+                if item in routine.body.defines_symbols:
+                    self._acc_copyout.add(item)
