33f572c115dfe734d18b6e606e92008f29e1eef8
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2023-03-13 17:05:07 +0000

    Rudimentary stack injection in driver

--- transformations/tests/test_pool_allocator.py
+++ transformations/tests/test_pool_allocator.py
@@ -9,7 +9,7 @@ from shutil import rmtree
 
 import pytest
 from loki import (
-    gettempdir, Scheduler, SchedulerConfig,
+    gettempdir, Scheduler, SchedulerConfig, Dimension,
     FindNodes,
     CallStatement, Assignment
 )
@@ -18,28 +18,51 @@ from conftest import available_frontends
 from transformations.pool_allocator import TemporariesPoolAllocatorTransformation
 
 
+@pytest.fixture(scope='module', name='horizontal')
+def fixture_horizontal():
+    return Dimension(name='horizontal', size='nlon', index='jl', bounds=('start', 'end'))
+
+
+@pytest.fixture(scope='module', name='vertical')
+def fixture_vertical():
+    return Dimension(name='vertical', size='nz', index='jk')
+
+
+@pytest.fixture(scope='module', name='blocking')
+def fixture_blocking():
+    return Dimension(name='blocking', size='nb', index='b')
+
+
+@pytest.mark.parametrize('generate_driver_stack', [False, True])
 @pytest.mark.parametrize('frontend', available_frontends())
-def test_pool_allocator_temporaries(frontend):
-    fcode_driver = """
-subroutine driver(KLON, KLEV, NGPBLK, FIELD1, FIELD2)
-    use stack_mod, only: stack
-    use kernel_mod, only: kernel
-    implicit none
-    INTEGER, PARAMETER :: JPRB = SELECTED_REAL_KIND(13,300)
-    INTEGER, INTENT(IN) :: KLON, KLEV, NGPBLK
-    real(kind=jprb), intent(inout) :: field1(klon, ngpblk)
-    real(kind=jprb), intent(inout) :: field2(klon, klev, ngpblk)
-    integer :: jblk
+def test_pool_allocator_temporaries(frontend, generate_driver_stack, horizontal, vertical, blocking):
+    fcode_stack_decl = """
     integer :: istsz
     REAL(KIND=JPRGB), ALLOCATABLE :: PSTACK(:, :)
     type(stack) :: ylstack
 
-    istsz = (klev+1) * klon
-    ALLOCATE(PSTACK(ISTSZ, NGPBLK))
-    do jblk=1,ngpblk
-        ylstack%l = loc(pstack, (1, jblk))
+    istsz = (nz+1) * nlon
+    ALLOCATE(PSTACK(ISTSZ, nb))
+    """
+    fcode_stack_assign = """
+        ylstack%l = loc(pstack, (1, b))
         ylstack%u = ylstack%l + 8 * istsz
-        call KERNEL(klon, klev, field1(:,jblk), field2(:,:,jblk))
+    """
+
+    fcode_driver = f"""
+subroutine driver(NLON, NZ, NGPBLK, FIELD1, FIELD2)
+    use stack_mod, only: stack
+    use kernel_mod, only: kernel
+    implicit none
+    INTEGER, PARAMETER :: JPRB = SELECTED_REAL_KIND(13,300)
+    INTEGER, INTENT(IN) :: NLON, NZ, NGPBLK
+    real(kind=jprb), intent(inout) :: field1(nlon, nb)
+    real(kind=jprb), intent(inout) :: field2(nlon, nz, nb)
+    integer :: b
+    {fcode_stack_decl if not generate_driver_stack else ''}
+    do b=1,nb
+        {fcode_stack_assign if not generate_driver_stack else ''}
+        call KERNEL(nlon, nz, field1(:,b), field2(:,:,b))
     end do
 end subroutine driver
     """.strip()
@@ -47,19 +70,19 @@ end subroutine driver
 module kernel_mod
     implicit none
 contains
-    subroutine kernel(klon, klev, field1, field2)
+    subroutine kernel(start, end, nlon, nz, field1, field2)
         implicit none
         integer, parameter :: jprb = selected_real_kind(13,300)
-        integer, intent(in) :: klon, klev
-        real(kind=jprb), intent(inout) :: field1(klon)
-        real(kind=jprb), intent(inout) :: field2(klon,klev)
-        real(kind=jprb) :: tmp1(klon)
-        real(kind=jprb) :: tmp2(klon, klev)
+        integer, intent(in) :: start, end, nlon, nz
+        real(kind=jprb), intent(inout) :: field1(nlon)
+        real(kind=jprb), intent(inout) :: field2(nlon,nz)
+        real(kind=jprb) :: tmp1(nlon)
+        real(kind=jprb) :: tmp2(nlon, nz)
         integer :: jk, jl
 
-        do jk=1,klev
+        do jk=1,nz
             tmp1(jl) = 0.0_jprb
-            do jl=1,klon
+            do jl=start,end
                 tmp2(jl, jk) = field2(jl, jk)
                 tmp1(jl) = field2(jl, jk)
             end do
@@ -87,7 +110,9 @@ end module kernel_mod
         }]
     }
     scheduler = Scheduler(paths=[basedir], config=SchedulerConfig.from_dict(config), frontend=frontend)
-    transformation = TemporariesPoolAllocatorTransformation()
+    transformation = TemporariesPoolAllocatorTransformation(
+        horizontal=horizontal, vertical=vertical, block_dim=blocking
+    )
     scheduler.process(transformation=transformation, reverse=True)
 
     #
@@ -98,12 +123,16 @@ end module kernel_mod
     # Has the stack been added to the call statement?
     calls = FindNodes(CallStatement).visit(driver.body)
     assert len(calls) == 1
-    assert calls[0].arguments == ('klon', 'klev', 'field1(:,jblk)', 'field2(:,:,jblk)', 'ylstack')
+    assert calls[0].arguments == ('nlon', 'nz', 'field1(:,b)', 'field2(:,:,b)', 'ylstack')
 
     #
     # A few checks on the kernel
     #
     kernel = scheduler['kernel_mod#kernel'].routine
+
+    # Has the stack module been imported?
+    assert any(import_.module.lower() == 'stack_mod' for import_ in kernel.imports)
+    assert 'stack' in kernel.imported_symbols
 
     # Has the stack been added to the arguments?
     assert 'ydstack' in kernel.arguments
@@ -146,5 +175,8 @@ end module kernel_mod
     # Check for stack size safegurads in generated code
     assert fcode.lower().count('if (ylstack%l > ylstack%u)') == 2
     assert fcode.lower().count('stop') == 2
+
+    if generate_driver_stack:
+        breakpoint()
 
     rmtree(basedir)
--- transformations/transformations/pool_allocator.py
+++ transformations/transformations/pool_allocator.py
@@ -6,25 +6,45 @@
 # nor does it submit to any jurisdiction.
 
 from loki import (
+    as_tuple,
     Transformation, FindNodes, Transformer,
     SymbolAttributes, BasicType, DerivedType,
     Variable, Array, Sum, Literal, Product, InlineCall, Comparison, RangeIndex,
-    Intrinsic, Assignment, Conditional, CallStatement, Import
+    Intrinsic, Assignment, Conditional, CallStatement, Import, Allocation, Deallocation,
+    Loop
 )
 
 __all__ = ['TemporariesPoolAllocatorTransformation']
 
 
 class TemporariesPoolAllocatorTransformation(Transformation):
+    """
+    Parameters
+    ----------
+    horizontal : :any:`Dimension`
+        :any:`Dimension` object describing the variable conventions used in code
+        to define the horizontal data dimension and iteration space.
+    vertical : :any:`Dimension`
+        :any:`Dimension` object describing the variable conventions used in code
+        to define the vertical dimension, as needed to decide array privatization.
+    block_dim : :any:`Dimension`
+        Optional ``Dimension`` object to define the blocking dimension
+        to use for hoisted column arrays if hoisting is enabled.
+    """
 
-    def __init__(self, stack_module_name='STACK_MOD', stack_type_name='STACK', stack_ptr_name='L',
-                 stack_size_name='U', stack_storage_name='PSTACK',
+    def __init__(self, horizontal, vertical, block_dim,
+                 stack_module_name='STACK_MOD', stack_type_name='STACK', stack_ptr_name='L',
+                 stack_end_name='U', stack_size_name='ISTSZ', stack_storage_name='PSTACK',
                  stack_arg_name='YDSTACK', stack_var_name='YLSTACK', pointer_var_name_pattern='IP_{name}',
                  **kwargs):
         super().__init__(**kwargs)
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.block_dim = block_dim
         self.stack_module_name = stack_module_name
         self.stack_type_name = stack_type_name
         self.stack_ptr_name = stack_ptr_name
+        self.stack_end_name = stack_end_name
         self.stack_size_name = stack_size_name
         self.stack_storage_name = stack_storage_name
         self.stack_arg_name = stack_arg_name
@@ -81,34 +101,58 @@ class TemporariesPoolAllocatorTransformation(Transformation):
             scope=routine
         )
 
-    def _get_stack_size(self, routine):
+    def _get_stack_end(self, routine):
         return Variable(
-            name=f'{self.stack_var_name}%{self.stack_size_name}',
+            name=f'{self.stack_var_name}%{self.stack_end_name}',
             parent=self._get_stack_var(routine),
             scope=routine
         )
 
-    def _get_stack_storage(self, routine):
+    def _get_stack_storage_and_size(self, routine):
+        body_prepend = []
+        body_append = []
+        variables_append = []
+        if self.stack_size_name in routine.variables:
+            stack_size = routine.variable_map[self.stack_size_name]
+        else:
+            stack_size = Variable(name=self.stack_size_name, type=SymbolAttributes(BasicType.INTEGER))
+            stack_size_assign = Assignment(
+                lhs=stack_size,
+                rhs=Product((
+                    Literal(50),
+                    Variable(name=self.horizontal.size, scope=routine),
+                    Variable(name=self.vertical.size, scope=routine)
+                ))
+            )
+            variables_append += [stack_size]
+            body_prepend += [stack_size_assign]
         if self.stack_storage_name in routine.variables:
-            return routine.variable_map[self.stack_storage_name]
-
-        stack_type = SymbolAttributes(
-            dtype=BasicType.REAL,
-            kind=Variable(name='JPRB', scope=routine),
-            shape=(
-                Product((Literal(5000), Variable(name='KLON', scope=routine))),
-                Variable(name='NGPBLK', scope=routine)
-            ),
-            allocatable=True,
-        )
-        stack_storage = Variable(
-            name=self.stack_storage_name, type=stack_type,
-            dimensions=(RangeIndex((None, None)), RangeIndex((None, None))),
-            scope=routine
-        )
-        routine.variables += (stack_storage,)
-        return stack_storage
-
+            stack_storage = routine.variable_map[self.stack_storage_name]
+        else:
+            stack_type = SymbolAttributes(
+                dtype=BasicType.REAL,
+                kind=Variable(name='JPRB', scope=routine),
+                shape=(RangeIndex((None, None)), RangeIndex((None, None))),
+                allocatable=True,
+            )
+            stack_storage = Variable(
+                name=self.stack_storage_name, type=stack_type,
+                dimensions=stack_type.shape, scope=routine
+            )
+            variables_append += [stack_storage]
+            stack_alloc = Allocation(variables=(stack_storage.clone(dimensions=(  # pylint: disable=no-member
+                stack_size, Variable(name=self.block_dim.size, scope=routine)
+            )),))
+            body_prepend += [stack_alloc]
+            stack_dealloc = Deallocation(variables=(stack_storage.clone(dimensions=None),))  # pylint: disable=no-member
+            body_append += [stack_dealloc]
+        if variables_append:
+            routine.variables += as_tuple(variables_append)
+        if body_prepend:
+            routine.body.prepend(body_prepend)
+        if body_append:
+            routine.body.append(body_append)
+        return stack_storage, stack_size
 
     def _create_stack_allocation(self, stack_ptr, stack_size, ptr_var, arr):
         ptr_assignment = Assignment(lhs=ptr_var, rhs=stack_ptr)
@@ -136,27 +180,38 @@ class TemporariesPoolAllocatorTransformation(Transformation):
 
         # Create Cray pointer declarations and "stack allocations"
         declarations = []
-        stack_ptr = Variable(name=f'{self.stack_var_name}%{self.stack_ptr_name}', parent=stack_var, scope=routine)
-        stack_size = Variable(name=f'{self.stack_var_name}%{self.stack_size_name}', parent=stack_var, scope=routine)
+        stack_ptr = self._get_stack_ptr(routine)
+        stack_end = self._get_stack_end(routine)
         for arr in temporary_arrays:
             ptr_var = Variable(name=self.pointer_var_name_pattern.format(name=arr.name), scope=routine)
             declarations += [Intrinsic(f'POINTER({ptr_var.name}, {arr.name})')]  # pylint: disable=no-member
-            allocations += self._create_stack_allocation(stack_ptr, stack_size, ptr_var, arr)
+            allocations += self._create_stack_allocation(stack_ptr, stack_end, ptr_var, arr)
 
         routine.spec.append(declarations)
         routine.body.prepend(allocations)
 
     def create_temporaries_pool_allocator(self, routine):
-        # TODO
-        # Create local stack var
-        # stack_storage = self._get_stack_storage(routine)
-        # stack_var = self._get_stack_var(routine)
-        # stack_alloc = Allocation(variables=stack_storage.clone(dimensions=stack_storage.shape))
-        # ptr_assignment = Assignment(
-        #     lhs=self._get_stack_ptr(routine),
-        #     rhs=InlineCall(Variable(name='LOC'), parameters=)
-        # )
-        pass
+        # Create and allocate the stack
+        stack_storage, stack_size = self._get_stack_storage_and_size(routine)
+        self._get_stack_var(routine)
+        stack_ptr = self._get_stack_ptr(routine)
+        stack_end = self._get_stack_end(routine)
+
+        # Find block loops and assign local stack pointers there
+        loop_map = {}
+        for loop in FindNodes(Loop).visit(routine.body):
+            if loop.variable == self.block_dim.index:
+                ptr_assignment = Intrinsic(
+                    f'{stack_ptr.name} = LOC({stack_storage.name}, (1, {loop.variable.name}))'  # pylint: disable=no-member
+                )
+                # Stack increment
+                stack_incr = Assignment(
+                    lhs=stack_end, rhs=Sum((stack_end, Product((stack_size, Literal(8)))))
+                )
+                loop_map[loop] = loop.clone(body=(ptr_assignment, stack_incr) + loop.body)
+
+        if loop_map:
+            routine.body = Transformer(loop_map).visit(routine.body)
 
     def inject_pool_allocator_into_calls(self, routine, targets):
         call_map = {}
