578c36015f62ceb1021b5f9b548722a0108577bc
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2023-03-17 17:09:13 +0000

    Test kernel calls in series

--- transformations/tests/test_pool_allocator.py
+++ transformations/tests/test_pool_allocator.py
@@ -28,7 +28,7 @@ def check_stack_module_import(routine):
     assert 'stack' in routine.imported_symbols
 
 
-def check_stack_created_in_driver(driver, stack_size, first_kernel_call, generate_driver_stack=True):
+def check_stack_created_in_driver(driver, stack_size, first_kernel_call, num_block_loops, generate_driver_stack=True):
     # Are stack size, storage and stack derived type declared?
     assert 'istsz' in driver.variables
     assert 'pstack(:,:)' in driver.variables
@@ -44,12 +44,12 @@ def check_stack_created_in_driver(driver, stack_size, first_kernel_call, generat
     assignments = FindNodes(Assignment).visit(driver.body)
     for assignment in assignments:
         if assignment.lhs == 'istsz':
-            assert simplify(assignment.rhs) == stack_size
+            assert str(simplify(assignment.rhs)).lower().replace(' ', '') == str(stack_size).lower().replace(' ', '')
             break
 
     # Check for stack assignment inside loop
     loops = FindNodes(Loop).visit(driver.body)
-    assert len(loops) == 1
+    assert len(loops) == num_block_loops
     assignments = FindNodes(Assignment).visit(loops[0].body)
     assert len(assignments) == 2
     assert assignments[0].lhs == 'ylstack%l'
@@ -95,7 +95,7 @@ subroutine driver(NLON, NZ, NGPBLK, FIELD1, FIELD2)
     {fcode_stack_decl if not generate_driver_stack else ''}
     do b=1,nb
         {fcode_stack_assign if not generate_driver_stack else ''}
-        call KERNEL(nlon, nz, field1(:,b), field2(:,:,b))
+        call KERNEL(1, nlon, nlon, nz, field1(:,b), field2(:,:,b))
     end do
     {fcode_stack_dealloc if not generate_driver_stack else ''}
 end subroutine driver
@@ -163,9 +163,9 @@ end module kernel_mod
     # Has the stack been added to the call statement?
     calls = FindNodes(CallStatement).visit(driver.body)
     assert len(calls) == 1
-    assert calls[0].arguments == ('nlon', 'nz', 'field1(:,b)', 'field2(:,:,b)', 'ylstack')
+    assert calls[0].arguments == ('1', 'nlon', 'nlon', 'nz', 'field1(:,b)', 'field2(:,:,b)', 'ylstack')
 
-    check_stack_created_in_driver(driver, 'nlon + nlon * nz', calls[0], generate_driver_stack)
+    check_stack_created_in_driver(driver, 'nlon + nlon * nz', calls[0], 1, generate_driver_stack)
 
     #
     # A few checks on the kernel
@@ -216,5 +216,168 @@ end module kernel_mod
     # Check for stack size safegurads in generated code
     assert fcode.lower().count('if (ylstack%l > ylstack%u)') == 2
     assert fcode.lower().count('stop') == 2
+
+    rmtree(basedir)
+
+
+
+@pytest.mark.parametrize('frontend', available_frontends())
+def test_pool_allocator_temporaries_kernel_sequence(frontend, blocking):
+    fcode_driver = """
+subroutine driver(NLON, NZ, NGPBLK, FIELD1, FIELD2)
+    use kernel_mod, only: kernel, kernel2
+    implicit none
+    INTEGER, PARAMETER :: JPRB = SELECTED_REAL_KIND(13,300)
+    INTEGER, INTENT(IN) :: NLON, NZ, NGPBLK
+    real(kind=jprb), intent(inout) :: field1(nlon, nb)
+    real(kind=jprb), intent(inout) :: field2(nlon, nz, nb)
+    integer :: b
+    do b=1,nb
+        call KERNEL(1, nlon, nlon, nz, field1(:,b), field2(:,:,b))
+    end do
+    do b=1,nb
+        call KERNEL2(1, nlon, nlon, nz, field2(:,:,b))
+    end do
+end subroutine driver
+    """.strip()
+    fcode_kernel = """
+module kernel_mod
+    implicit none
+contains
+    subroutine kernel(start, end, nlon, nz, field1, field2)
+        implicit none
+        integer, parameter :: jprb = selected_real_kind(13,300)
+        integer, intent(in) :: start, end, nlon, nz
+        real(kind=jprb), intent(inout) :: field1(nlon)
+        real(kind=jprb), intent(inout) :: field2(nlon,nz)
+        real(kind=jprb) :: tmp1(nlon)
+        real(kind=jprb) :: tmp2(nlon, nz)
+        integer :: jk, jl
+
+        do jk=1,nz
+            tmp1(jl) = 0.0_jprb
+            do jl=start,end
+                tmp2(jl, jk) = field2(jl, jk)
+                tmp1(jl) = field2(jl, jk)
+            end do
+            field1(jl) = tmp1(jl)
+        end do
+    end subroutine kernel
+
+    subroutine kernel2(start, end, nlon, nz, field2)
+        implicit none
+        integer, parameter :: jprb = selected_real_kind(13,300)
+        integer, intent(in) :: start, end, nlon, nz
+        real(kind=jprb), intent(inout) :: field2(nlon,nz)
+        real(kind=jprb) :: tmp1(nlon, nz), tmp2(nlon, nz)
+        integer :: jk, jl
+
+        do jk=1,nz
+            do jl=start,end
+                tmp1(jl, jk) = field2(jl, jk)
+                tmp2(jl, jk) = tmp1(jl, jk) + 1._jprb
+                field2(jl, jk) = tmp2(jl, jk)
+            end do
+        end do
+    end subroutine kernel2
+
+end module kernel_mod
+    """.strip()
+
+    basedir = gettempdir()/'test_pool_allocator_temporaries_kernel_sequence'
+    basedir.mkdir(exist_ok=True)
+    (basedir/'driver.F90').write_text(fcode_driver)
+    (basedir/'kernel_mod.F90').write_text(fcode_kernel)
+
+    config = {
+        'default': {
+            'mode': 'idem',
+            'role': 'kernel',
+            'expand': True,
+            'strict': True
+        },
+        'routine': [{
+            'name': 'driver',
+            'role': 'driver',
+        }]
+    }
+    scheduler = Scheduler(paths=[basedir], config=SchedulerConfig.from_dict(config), frontend=frontend)
+    transformation = TemporariesPoolAllocatorTransformation(block_dim=blocking)
+    scheduler.process(transformation=transformation, reverse=True)
+    kernel_item = scheduler['kernel_mod#kernel']
+    kernel2_item = scheduler['kernel_mod#kernel2']
+
+    assert transformation._key in kernel_item.trafo_data
+    assert kernel_item.trafo_data[transformation._key] == 'nlon + nlon * nz'
+    assert kernel2_item.trafo_data[transformation._key] == '2 * nlon * nz'
+    assert all(v.scope is None for v in FindVariables().visit(kernel_item.trafo_data[transformation._key]))
+    assert all(v.scope is None for v in FindVariables().visit(kernel2_item.trafo_data[transformation._key]))
+
+    #
+    # A few checks on the driver
+    #
+    driver = scheduler['#driver'].routine
+
+    # Has the stack module been imported?
+    check_stack_module_import(driver)
+
+    # Has the stack been added to the call statements?
+    calls = FindNodes(CallStatement).visit(driver.body)
+    assert len(calls) == 2
+    assert calls[0].arguments == ('1', 'nlon', 'nlon', 'nz', 'field1(:,b)', 'field2(:,:,b)', 'ylstack')
+    assert calls[1].arguments == ('1', 'nlon', 'nlon', 'nz', 'field2(:,:,b)', 'ylstack')
+
+    check_stack_created_in_driver(driver, 'max(nlon + nlon * nz, 2 * nlon * nz)', calls[0], 2)
+
+    #
+    # A few checks on the kernel
+    #
+    for item in [kernel_item, kernel2_item]:
+        kernel = item.routine
+
+        # Has the stack module been imported?
+        check_stack_module_import(kernel)
+
+        # Has the stack been added to the arguments?
+        assert 'ydstack' in kernel.arguments
+
+        # Is it being assigned to a local variable?
+        assert 'ylstack' in kernel.variables
+
+        # Let's check for the relevant "allocations" happening in the right order
+        assign_idx = {}
+        for idx, assign in enumerate(FindNodes(Assignment).visit(kernel.body)):
+            if assign.lhs == 'ylstack' and assign.rhs == 'ydstack':
+                # Local copy of stack status
+                assign_idx['stack_assign'] = idx
+            elif assign.lhs == 'ip_tmp1' and assign.rhs == 'ylstack%l':
+                # Assign Cray pointer for tmp1
+                assign_idx['tmp1_ptr_assign'] = idx
+            elif assign.lhs == 'ip_tmp2' and assign.rhs == 'ylstack%l':
+                # Assign Cray pointer for tmp2
+                assign_idx['tmp2_ptr_assign'] = idx
+            elif assign.lhs == 'ylstack%l' and 'ylstack%l' in assign.rhs and 'size' in assign.rhs and 'tmp1' in assign.rhs:
+                # Stack increment for tmp1
+                assign_idx['tmp1_stack_incr'] = idx
+            elif assign.lhs == 'ylstack%l' and 'ylstack%l' in assign.rhs and 'tmp2' in assign.rhs and 'tmp2' in assign.rhs:
+                # Stack increment for tmp2
+                assign_idx['tmp2_stack_incr'] = idx
+
+        expected_assign_in_order = [
+            'stack_assign', 'tmp1_ptr_assign', 'tmp1_stack_incr', 'tmp2_ptr_assign', 'tmp2_stack_incr'
+        ]
+        assert set(expected_assign_in_order) == set(assign_idx.keys())
+
+        for assign1, assign2 in zip(expected_assign_in_order, expected_assign_in_order[1:]):
+            assert assign_idx[assign2] > assign_idx[assign1]
+
+        # Check for pointer declarations in generated code
+        fcode = kernel.to_fortran()
+        assert 'pointer(ip_tmp1, tmp1)' in fcode.lower()
+        assert 'pointer(ip_tmp2, tmp2)' in fcode.lower()
+
+        # Check for stack size safegurads in generated code
+        assert fcode.lower().count('if (ylstack%l > ylstack%u)') == 2
+        assert fcode.lower().count('stop') == 2
 
     rmtree(basedir)
