03a6f978b0debd0fe2f1f1bbcd0e5dc02d281c9e
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2023-09-06 23:15:36 +0100

    Add item filter to file graph scheduler traversal

--- scripts/loki_transform.py
+++ scripts/loki_transform.py
@@ -303,8 +303,14 @@ def convert(
     scheduler.process(transformation=dependency, use_file_graph=True, recurse_to_contained_nodes=True)
 
     # Write out all modified source files into the build directory
-    scheduler.process(transformation=FileWriteTransformation(builddir=out_path, mode=mode, cuf='cuf' in mode),
-                      use_file_graph=True)
+    if global_var_offload:
+        item_filter = (SubroutineItem, GlobalVarImportItem)
+    else:
+        item_filter = SubroutineItem
+    scheduler.process(
+        transformation=FileWriteTransformation(builddir=out_path, mode=mode, cuf='cuf' in mode),
+        use_file_graph=True, item_filter=item_filter
+    )
 
 
 @cli.command()
