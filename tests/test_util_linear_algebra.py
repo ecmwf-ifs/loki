59f41bfee7071dba6928d69dcc66a5aadf1f1b08
joscao <62660664+joscao@users.noreply.github.com>
2023-10-23 15:01:44 +0200

    Test enforcing conditions and division operations

--- tests/test_util_linear_algebra.py
+++ tests/test_util_linear_algebra.py
@@ -5,6 +5,7 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 import pytest
+from math import gcd
 import numpy as np
 
 from loki.analyse.util_linear_algebra import (
@@ -63,6 +64,50 @@ def test_generate_row_echelon_form(matrix, result):
 
     assert np.allclose(generate_row_echelon_form(matrix), result)
 
+@pytest.mark.parametrize(
+    "matrix, result",
+    [
+        ([[2, 0, 1], [0, 2, 0]], [[1,0,0],[0,1,0]]),
+        ([[1, -2, 1, 0], [3, 2, 1, 5]], [[1,-2,1,0],[0,1,-1,0]]),
+        ([[1, -1, -10]], [[1, -1, -10]]),
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[1, 2, 3], [0, 1, 2], [0, 0, 0]]),
+        ([[0, 1, 0], [0, 0, 1], [0, 0, 0]], [[0, 1, 0], [0, 0, 1], [0, 0, 0]]),
+        ([[2, 4, 6, 8], [1, 2, 3, 4], [3, 6, 9, 12]], [[1, 2, 3, 4], [0, 0, 0, 0], [0, 0, 0, 0]]),
+    ],
+)
+def test_enforce_integer_arithmetics_for_row_echelon_form(matrix, result):
+    matrix = np.array(matrix, dtype=float)
+    result = np.array(result, dtype=float)
+
+    assert np.allclose(generate_row_echelon_form(matrix, division_operator=lambda x,y: x//y), result)
+
+
+def _raise_assertion_error(A):
+    raise ValueError()
+
+def _require_gcd_condition(A):
+    """Check that gcd condition of linear Diophantine equation is satisfied"""
+    if A[0, -1] % gcd(*A[0, :-1]) != 0:
+        raise ValueError()
+
+@pytest.mark.parametrize(
+    "matrix, condition, result",
+    [
+        ([[1,2,3],[4,5,6]], _raise_assertion_error, None),
+        ([[2,0,0,-2,-20],[0,2,-2,0,-22]],_require_gcd_condition,[[1,0,0,-1,-10],[0,1,-1,0,-11]]),
+        ([[2,0,0,-2,-20],[0,2,-2,0,-21]],_require_gcd_condition,None),
+    ]
+)
+def test_require_conditions(matrix, condition, result):
+    matrix = np.array(matrix)
+
+    if result is None:
+        with pytest.raises(ValueError):
+            _ = generate_row_echelon_form(matrix, conditional_check=condition)
+    else:
+        result = np.array(result)
+        assert np.allclose(generate_row_echelon_form(matrix, conditional_check=condition), result)
 
 @pytest.mark.parametrize(
     "matrix, rhs, expected_lower, expected_upper",
