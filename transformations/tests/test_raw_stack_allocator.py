96487e4377957352668017ae9b79a04e61b948ac
Rolf Heilemann Myhre <rolfhm@met.no>
2024-01-10 17:23:16 +0100

    start testing

---
+++ transformations/tests/test_raw_stack_allocator.py
@@ -0,0 +1,247 @@
+# (C) Copyright 2018- ECMWF.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+from shutil import rmtree
+
+import pytest
+
+from loki.tools import gettempdir
+from loki.dimension import Dimension
+from loki.bulk import Scheduler, SchedulerConfig
+from loki.frontend.util import OMNI, FP, OFP
+from loki.backend.fgen import fgen
+
+from conftest import available_frontends
+
+from transformations.raw_stack_allocator import TemporariesRawStackTransformation
+
+
+@pytest.fixture(scope='module', name='block_dim')
+def fixture_block_dim():
+    return Dimension(name='block_dim', size='nb', index='b')
+
+@pytest.fixture(scope='module', name='horizontal')
+def fixture_horizontal():
+    return Dimension(name='horizontal', size='nlon', index='jl', bounds=('jstart', 'jend'))
+
+@pytest.mark.parametrize('frontend', available_frontends())
+def test_raw_stack_allocator_temporaries(frontend, block_dim, horizontal):
+
+    fcode_driver = f"""
+module driver_mod
+  contains
+  subroutine driver(nlon, klev, nb, ydml_phy_mf)
+
+    use parkind1, only: jpim, jprb
+
+    use model_physics_mf_mod, only: model_physics_mf_type
+    use kernel1_mod, only: kernel1
+
+    implicit none
+
+    type(model_physics_mf_type), intent(in) :: ydml_phy_mf
+
+    integer(kind=jpim), intent(in) :: nlon
+    integer(kind=jpim), intent(in) :: klev
+    integer(kind=jpim), intent(in) :: nb
+
+    integer(kind=jpim) :: jstart
+    integer(kind=jpim) :: jend
+
+    integer(kind=jpim) :: b
+
+    real(kind=jprb), dimension(nlon, klev) :: zzz
+
+    jstart = 1
+    jend = nlon
+
+    do b = 1, nb
+
+        call kernel1(ydml_phy_mf, nlon, klev, jstart, jend, zzz)
+
+    enddo
+
+  end subroutine driver
+end module driver_mod
+    """.strip()
+
+    fcode_kernel1 = f"""
+module kernel1_mod
+  contains
+  subroutine kernel1(ydml_phy_mf, nlon, klev, jstart, jend, pzz)
+
+    use parkind1, only: jpim, jprb
+
+    use model_physics_mf_mod, only: model_physics_mf_type
+    use kernel2_mod, only: kernel2
+    use kernel3_mod, only: kernel3
+
+    implicit none
+
+    type(model_physics_mf_type), intent(in) :: ydml_phy_mf
+
+    integer(kind=jpim), intent(in) :: nlon
+    integer(kind=jpim), intent(in) :: klev
+
+    integer(kind=jpim), intent(in) :: jstart
+    integer(kind=jpim), intent(in) :: jend
+
+    real(kind=jprb), intent(in), dimension(nlon, klev) :: pzz
+
+    real(kind=jprb), dimension(nlon, klev) :: zzx
+    logical, dimension(nlon, klev) :: zzl
+
+    integer :: jl, jlev
+
+    do jl =1, nlon
+      do jlev = 1, klev
+        zzl(jl, jlev) = .false.
+        zzx(jl, jlev) = pzz(jl, jlev)
+      enddo
+    enddo
+
+    call kernel2(ydml_phy_mf%yrphy3, nlon, klev, jstart, jend)
+    call kernel3(ydml_phy_mf%yrphy3, nlon, klev, jstart, jend, pzz)
+
+  end subroutine kernel1
+end module kernel1_mod
+    """.strip()
+
+    fcode_kernel2 = f"""
+module kernel2_mod
+  contains
+  subroutine kernel2(ydphy3, nlon, klev, jstart, jend)
+
+      use parkind1, only: jpim, jprb
+
+      use yomphy3, only:  tphy3
+
+      implicit none
+
+      type(tphy3), intent(in) :: ydphy3
+
+      integer(kind=jpim), intent(in) :: nlon
+      integer(kind=jpim), intent(in) :: klev
+      integer(kind=jpim), intent(in) :: jstart
+      integer(kind=jpim), intent(in) :: jend
+
+      integer(kind=jpim) :: jb, jlev, jl
+
+      real(kind=jprb) :: zde1(nlon, 0:klev, ydphy3%n_spband)
+      real(kind=jprb) :: zde2(nlon, klev, ydphy3%n_spband)
+
+      do jb = 1, ydphy3%n_spband
+        do jlev = 1, klev
+          do jl = jstart, jend
+
+            zde1(jl, jlev, jb) = 0._jprb
+            zde2(jl, jlev, jb) = 0._jprb
+
+          enddo
+        enddo
+      enddo
+
+  end subroutine kernel2
+end module kernel2_mod
+    """.strip()
+
+    fcode_kernel3 = f"""
+module kernel3_mod
+  contains
+  subroutine kernel3(ydphy3, nlon, klev, jstart, jend, pzz)
+
+      use parkind1, only: jpim, jprb
+
+      use yomphy3, only:  tphy3
+
+      implicit none
+
+      type(tphy3), intent(in) :: ydphy3
+
+      integer(kind=jpim), intent(in) :: nlon
+      integer(kind=jpim), intent(in) :: klev
+      integer(kind=jpim), intent(in) :: jstart
+      integer(kind=jpim), intent(in) :: jend
+
+      real(kind=jprb), intent(in), dimension(nlon, klev) :: pzz
+
+      integer(kind=jpim) :: jb, jlev, jl
+
+      real(kind=jprb) :: zde1(nlon, 0:klev, ydphy3%n_spband)
+      real(kind=jprb) :: zde2(nlon, klev, ydphy3%n_spband)
+      real(kind=jprb) :: zde3(nlon, 1:klev)
+
+
+      do jb = 1, ydphy3%n_spband
+        zde1(:, 0, jb) = 0._jprb
+        zde2(:, :, jb) = 0._jprb
+        do jlev = 1, klev
+          do jl = jstart, jend
+
+            zde1(jl, jlev, jb) = 1._jprb
+            zde2(jl, jlev, jb) = 0._jprb
+
+          enddo
+        enddo
+      enddo
+
+      zde3(1:nlon,1:klev) = pzz
+
+  end subroutine kernel3
+end module kernel3_mod
+    """.strip()
+
+    basedir = gettempdir()/'test_pool_allocator_temporaries'
+    basedir.mkdir(exist_ok=True)
+    (basedir/'driver.F90').write_text(fcode_driver)
+    (basedir/'kernel1_mod.F90').write_text(fcode_kernel1)
+    (basedir/'kernel2_mod.F90').write_text(fcode_kernel2)
+    (basedir/'kernel3_mod.F90').write_text(fcode_kernel3)
+
+    config = {
+        'default': {
+            'mode': 'idem',
+            'role': 'kernel',
+            'expand': True,
+            'strict': True
+        },
+        'routines': {
+            'driver': {'role': 'driver'}
+        }
+    }
+
+    scheduler = Scheduler(paths=[basedir], config=SchedulerConfig.from_dict(config), frontend=frontend)
+
+    if frontend == OMNI:
+        for item in scheduler.items:
+            normalize_range_indexing(item.routine)
+
+    transformation = TemporariesRawStackTransformation(block_dim=block_dim, horizontal=horizontal)
+    scheduler.process(transformation=transformation)
+
+    kernel1_item = scheduler['kernel1_mod#kernel1']
+
+    assert transformation._key in kernel1_item.trafo_data
+
+    real_stack_size = 'MAX(klev + ydml_phy_mf%yrphy3%n_spband + 2*klev*ydml_phy_mf%yrphy3%n_spband, 2*klev + ydml_phy_mf%yrphy3%n_spband + 2*klev*ydml_phy_mf%yrphy3%n_spband)'
+
+    logical_stack_size = 'klev'
+
+    stack_dict = kernel1_item.trafo_data[transformation._key]['stack_dict']
+    for dtype in stack_dict:
+        print(dtype)
+        for kind in stack_dict[dtype]:
+            print(kind)
+            print(fgen(stack_dict[dtype][kind]))
+            print(real_stack_size)
+            print(fgen(stack_dict[dtype][kind]) == real_stack_size)
+            print(fgen(stack_dict[dtype][kind]) == logical_stack_size)
+            print()
+        print()
+
+    rmtree(basedir)
+
