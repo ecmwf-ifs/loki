5b1dba53cb7148646ce82c472b97f5371bb86392
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2024-03-07 22:43:38 +0100

    Make ModuleWrapTransformation work with ExternalItem

--- tests/sources/projB/external/ext_driver_mod.f90
+++ tests/sources/projB/external/ext_driver_mod.f90
@@ -1,13 +1,19 @@
+#ifdef HAVE_EXT_DRIVER_MODULE
 module ext_driver_mod
-  use ext_kernel_mod, only: jprb, ext_kernel
+  implicit none
 
 contains
+#endif
 
   subroutine ext_driver(vector, matrix)
+    use ext_kernel_mod, only: jprb, ext_kernel
+    implicit none
     real(kind=jprb), intent(inout) :: vector(:)
     real(kind=jprb), intent(inout) :: matrix(:, :)
 
     call ext_kernel(vector, matrix)
   end subroutine ext_driver
 
+#ifdef HAVE_EXT_DRIVER_MODULE
 end module ext_driver_mod
+#endif
