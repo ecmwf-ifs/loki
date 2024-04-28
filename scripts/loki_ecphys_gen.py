22f01280166e8d88d6a6a55f9a84a624a62967fe
Michael Lange <Michael.Lange@ecmwf.int>
2024-04-28 05:36:02 +0000

    ecphys-gen: Add mechanism to replace DIMS symbols

--- scripts/loki_ecphys_gen.py
+++ scripts/loki_ecphys_gen.py
@@ -16,11 +16,35 @@ import click
 from pathlib import Path
 from codetiming import Timer
 
-from loki import Sourcefile, FindNodes, CallStatement, Transformer, info
+from loki import (
+    Sourcefile, FindNodes, CallStatement, Transformer, info
+)
+from loki.expression import (
+    symbols as sym, parse_expr, SubstituteExpressions
+)
+from loki.tools import as_tuple, flatten
+
 from loki.transformations.inline import inline_subroutine_calls
 from loki.transformations.sanitise import transform_sequence_association_append_map
 from loki.transformations.remove_code import do_remove_marked_regions
 from loki.transformations.build_system import ModuleWrapTransformation
+
+
+def substitute_spec_symbols(mapping, routine):
+    """
+    Do symbol substitution on the spec/declartion of a subroutine from
+    a given string mapping via :any:`parse_expr`.
+
+    Importantly, this will use the given :any:`Subroutine` as the scope.
+    """
+    symbol_map = {
+        parse_expr(k, scope=routine): parse_expr(v, scope=routine)
+        for k, v in mapping.items()
+    }
+
+    routine.spec = SubstituteExpressions(symbol_map).visit(routine.spec)
+
+    return routine
 
 
 @click.group()
@@ -52,6 +76,19 @@ def inline(source, build, remove_regions):
 
     # Clone original and change subroutine name
     ec_phys_fc = ec_phys_drv.clone(name='EC_PHYS_FC')
+
+    # Substitute symbols that do not exist in the caller context after inlining
+    substitute_spec_symbols(routine=ec_phys, mapping = {
+        'DIMS%KLON': 'YDGEOMETRY%YRDIM%NPROMA',
+        'DIMS%KLEV': 'YDGEOMETRY%YRDIMV%NFLEVG',
+        'DIMS%KLEVS': 'YDSURF%YSP_SBD%NLEVS',
+        }
+    )
+    substitute_spec_symbols(routine=callpar, mapping = {
+        'KDIM%KLON': 'YDGEOMETRY%YRDIM%NPROMA',
+        'KDIM%KLEV': 'YDGEOMETRY%YRDIMV%NFLEVG',
+        }
+    )
 
     with Timer(logger=info, text=lambda s: f'[Loki::EC-Physics] Inlined EC_PHYS in {s:.2f}s'):
         # First, get the outermost call
