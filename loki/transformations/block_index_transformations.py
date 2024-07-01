51d5f534cdec4d7e4ac6877407a66d1c6ecdc89b
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2024-07-01 13:46:24 +0200

    BlockIndexInject: Exclude non-target calls from arg_map (fixes #336)

--- loki/transformations/block_index_transformations.py
+++ loki/transformations/block_index_transformations.py
@@ -41,14 +41,14 @@ class BlockViewToFieldViewTransformation(Transformation):
         enddo
 
     As the rank of ``my_struct%p_field`` is one greater than that of ``my_struct%p``, we would need to also apply
-    the :any:`InjectBlockIndexTransformation` to obtain semantically correct code: 
+    the :any:`InjectBlockIndexTransformation` to obtain semantically correct code:
 
     .. code-block:: fortran
 
         do jlon=1,nproma
           mystruct%p_field(jlon,:,ibl) = 0.
         enddo
-    
+
     Specific arrays in individual routines can also be marked for exclusion from this transformation by assigning
     them to the `exclude_arrays` list in the :any:`SchedulerConfig`.
 
@@ -372,13 +372,14 @@ class InjectBlockIndexTransformation(Transformation):
     def process_body(self, body, block_index, targets, exclude_arrays):
         # The logic for callstatement args differs from other variables in the body,
         # so we build a list to filter
-        call_args = [a for call in FindNodes(ir.CallStatement).visit(body) for a in call.arg_map.values()]
+        call_args = []
 
         # First get rank mismatched call statement args
         vmap = {}
         for call in FindNodes(ir.CallStatement).visit(body):
             if call.name in targets:
                 for dummy, arg in call.arg_map.items():
+                    call_args += [arg]
                     arg_rank = self.get_call_arg_rank(arg)
                     dummy_rank = len(getattr(dummy, 'shape', ()))
                     if arg_rank - 1 == dummy_rank:
