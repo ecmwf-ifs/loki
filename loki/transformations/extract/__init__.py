3ef33edd5164af1b6b1f59859d108e3224e684c0
Michael Lange <Michael.Lange@ecmwf.int>
2024-09-12 18:42:16 +0000

    Transformation: Add `ExtractTransformation` and tests

--- loki/transformations/extract/__init__.py
+++ loki/transformations/extract/__init__.py
@@ -7,7 +7,75 @@
 """
 Transformations sub-package that provides various forms of
 source-code extraction into standalone :any:`Subroutine` objects.
+
+The various extractions mechanisms are provided as standalone utility
+methods, or via the :any:`ExtractTransformation` class for for batch
+processing.
+
+These utilities represent the conceptual inverse operation to
+"inlining", as done by the :any:`InlineTransformation`.
 """
 
 from loki.transformations.extract.internal import * # noqa
 from loki.transformations.extract.marked import * # noqa
+
+from loki.batch import Transformation
+
+
+__all__ = ['ExtractTransformation']
+
+
+class ExtractTransformation(Transformation):
+    """
+    :any:`Transformation` class to apply several types of source
+    extraction when batch-processing large source trees via the
+    :any:`Scheduler`.
+
+    Parameters
+    ----------
+    inline_internals : bool
+        Extract internal procedure (see :any:`extract_internal_procedures`);
+        default: False.
+    inline_marked : bool
+        Extract :any:`Subroutine` objects marked by pragma annotations
+        (see :any:`extract_marked_subroutines`); default: True.
+    """
+    def __init__(self, extract_internals=False, extract_marked=True):
+        self.extract_internals = extract_internals
+        self.extract_marked = extract_marked
+
+    def transform_module(self, module, **kwargs):
+        """
+        Extract internals procedures and marked subroutines and add
+        them to the given :any:`Module`.
+        """
+
+        # Extract internal (contained) procedures into standalone ones
+        if self.extract_internals:
+            for routine in module.subroutines:
+                new_routines = extract_internal_procedures(routine)
+                module.contains.append(new_routines)
+
+        # Extract pragma-marked code regions into standalone subroutines
+        if self.extract_marked:
+            for routine in module.subroutines:
+                new_routines = extract_marked_subroutines(routine)
+                module.contains.append(new_routines)
+
+    def transform_file(self, sourcefile, **kwargs):
+        """
+        Extract internals procedures and marked subroutines and add
+        them to the given :any:`Sourcefile`.
+        """
+
+        # Extract internal (contained) procedures into standalone ones
+        if self.extract_internals:
+            for routine in sourcefile.subroutines:
+                new_routines = extract_internal_procedures(routine)
+                sourcefile.ir.append(new_routines)
+
+        # Extract pragma-marked code regions into standalone subroutines
+        if self.extract_marked:
+            for routine in sourcefile.subroutines:
+                new_routines = extract_marked_subroutines(routine)
+                sourcefile.ir.append(new_routines)
