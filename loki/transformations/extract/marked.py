31a960375a76fe3ef77bbb13a17eb5df7454d65b
Michael Lange <Michael.Lange@ecmwf.int>
2024-09-25 15:06:23 +0000

    Transformation: Bug fix for `extract_amarked-region`
    Need to explicit give an empty tuple for kwargument when creating
    the CallStatement, as dataflow analysis will iterate it.

--- loki/transformations/extract/marked.py
+++ loki/transformations/extract/marked.py
@@ -102,7 +102,7 @@ def extract_subroutine(region, name, imports, intent_map=None):
 
     # Create the call according to the wrapped code region
     call_arguments = region_in_args + region_inout_args + region_out_args
-    call = CallStatement(name=Variable(name=name), arguments=call_arguments)
+    call = CallStatement(name=Variable(name=name), arguments=call_arguments, kwarguments=())
 
     return call, region_routine
 
