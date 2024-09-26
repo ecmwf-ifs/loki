018ce602164dac92d39c2b9f4e95e26af7c9bd53
Michael Lange <Michael.Lange@ecmwf.int>
2024-09-26 12:01:00 +0000

    ecphys-gen: Add simple mechanism to extract driver routines to files

--- scripts/loki_ecphys_gen.py
+++ scripts/loki_ecphys_gen.py
@@ -17,15 +17,18 @@ import click
 from codetiming import Timer
 
 from loki import config as loki_config, Sourcefile, Dimension, info
+from loki.analyse import dataflow_analysis_attached
 from loki.batch import Scheduler, Pipeline
 from loki.expression import symbols as sym
 from loki.ir import (
     nodes as ir, FindNodes, FindVariables, SubstituteExpressions,
-    Transformer
+    Transformer, pragma_regions_attached, is_loki_pragma,
+    get_pragma_parameters
 )
 
 from loki.transformations.build_system import ModuleWrapTransformation
 from loki.transformations.drhook import DrHookTransformation
+from loki.transformations.extract import outline_region
 from loki.transformations.inline import InlineTransformation
 from loki.transformations.parallel import (
     remove_openmp_regions, add_openmp_regions,
@@ -33,7 +36,9 @@ from loki.transformations.parallel import (
     remove_field_api_view_updates, add_field_api_view_updates,
     remove_explicit_firstprivatisation, create_explicit_firstprivatisation
 )
-from loki.transformations.remove_code import RemoveCodeTransformation
+from loki.transformations.remove_code import (
+    RemoveCodeTransformation, do_remove_unused_imports
+)
 from loki.transformations.sanitise import (
     SequenceAssociationTransformation, SubstituteExpressionTransformation,
     do_merge_associates, do_resolve_associates
@@ -142,6 +147,37 @@ def promote_temporary_arrays(routine, horizontal, blocking):
         )
         decl._update(symbols=symbols)
 
+
+def outline_driver_routines(routine):
+    """
+    Extracts driver routines and replaces them with an appropriate
+    :any:`CallStatement`.
+    """
+    imports = FindNodes(ir.Import).visit(routine.spec)
+    mapper = {}
+    driver_routines = []
+    with pragma_regions_attached(routine):
+        with dataflow_analysis_attached(routine):
+            for region in FindNodes(ir.PragmaRegion).visit(routine.body):
+                if not is_loki_pragma(region.pragma, starts_with='outline'):
+                    continue
+
+                # Name the external routine
+                parameters = get_pragma_parameters(region.pragma, starts_with='outline')
+                name = parameters['name']
+
+                call, region_routine = outline_region(region, name, imports, intent_map=parameters)
+
+                do_remove_unused_imports(region_routine)
+
+                driver_routines.append(region_routine)
+
+                # Replace region by call in original routine
+                mapper[region] = call
+
+            routine.body = Transformer(mapper=mapper).visit(routine.body)
+
+    return driver_routines
 
 @click.group()
 def cli():
@@ -326,6 +362,19 @@ def parallel(source, build, remove_block_loop, promote_local_arrays, log_level):
 
             # Re-insert explicit firstprivate copies
             create_explicit_firstprivatisation(ec_phys_parallel, fprivate_map=lcopies_firstprivates)
+
+    with Timer(logger=info, text=lambda s: f'[Loki::EC-Physics] Extracted driver routines in {s:.2f}s'):
+
+        driver_routines = outline_driver_routines(ec_phys_parallel)
+        for driver in driver_routines:
+            # Create a new source file for the extracted routine
+            filename = driver.name.lower() + '.F90'
+            sourcefile = Sourcefile(ir=ir.Section(driver), path=build/filename)
+            sourcefile.write()
+
+            # Add an implicit C-style import to the control-flow routine
+            imprt = ir.Import(module=f'{driver.name.lower()}.intfb.h', c_import=True)
+            ec_phys_parallel.spec.append(imprt)
 
     if promote_local_arrays:
         with Timer(logger=info, text=lambda s: f'[Loki::EC-Physics] Promoted local arrays in {s:.2f}s'):
