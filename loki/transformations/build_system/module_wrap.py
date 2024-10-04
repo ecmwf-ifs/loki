9246420702822f3e427095a998e50d72152acf6e
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2024-10-04 09:35:20 +0200

    MODULE_WRAP: fix bug in replace_ignore_items option

--- loki/transformations/build_system/module_wrap.py
+++ loki/transformations/build_system/module_wrap.py
@@ -110,7 +110,7 @@ class ModuleWrapTransformation(Transformation):
         """
 
         targets = tuple(str(t).lower() for t in as_tuple(kwargs.get('targets')))
-        if self.replace_ignore_items and (item := kwargs.get('item')):
+        if (item := kwargs.get('item')) and self.replace_ignore_items:
             targets += tuple(str(i).lower() for i in item.ignore)
 
         def _update_item(proc_name, module_name):
