3a5ae2f0599742510edc56a96e0720586c5f1a51
Michael Lange <Michael.Lange@ecmwf.int>
2024-10-18 13:54:20 +0000

    Parallel: Add utility to remove existing OpenMP parallel region
    Optionally, this utility can also replace these outer pragmas with
    `!$loki parallal` pragma markers.

--- loki/transformations/__init__.py
+++ loki/transformations/__init__.py
@@ -36,3 +36,4 @@ from loki.transformations.block_index_transformations import * # noqa
 from loki.transformations.split_read_write import * # noqa
 from loki.transformations.loop_blocking import * # noqa
 from loki.transformations.routine_signatures import * # noqa
+from loki.transformations.parallel import * # noqa
---
+++ loki/transformations/parallel/__init__.py
@@ -0,0 +1,13 @@
+# (C) Copyright 2018- ECMWF.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+"""
+Sub-package with utilities to remove, generate and manipulate parallel
+regions.
+"""
+
+from loki.transformations.parallel.openmp_region import * # noqa
