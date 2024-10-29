6151c1a6c6893cc3badf0489a44b0aa78791a3fd
Michael Lange <Michael.Lange@ecmwf.int>
2024-10-29 10:36:09 +0000

    Expression: Add test for `LokiIdentityMapper`

--- loki/expression/tests/test_mapper.py
+++ loki/expression/tests/test_mapper.py
@@ -8,8 +8,9 @@
 import pytest
 
 from loki import Subroutine, Scope
-from loki.expression import (
-    symbols as sym, parse_expr, ExpressionRetriever
+from loki.expression import symbols as sym, parse_expr
+from loki.expression.mappers import (
+    ExpressionRetriever, LokiIdentityMapper
 )
 from loki.frontend import available_frontends
 from loki.ir import nodes as ir, FindNodes
@@ -57,3 +58,36 @@ end subroutine test_expr_retriever
     # Cannot determine Scalar without declarations, so check for deferred
     assert ExpressionRetriever(q_deferred).retrieve(expr) == ['a', 'c', 'a']
     assert ExpressionRetriever(q_literal).retrieve(expr) == [5, 4]
+
+
+@pytest.mark.parametrize('frontend', available_frontends())
+def test_identity_mapper(frontend):
+    """
+    Test for :any:`LokiIdentityMapper`, in particular deep-copying
+    expression nodes.
+    """
+
+    fcode = """
+subroutine test_expr_retriever(n, a, b, c)
+  integer, intent(inout) :: n, a, b(n), c
+
+  a = 5 * a + 4 * b(c) + a
+end subroutine test_expr_retriever
+"""
+    routine = Subroutine.from_source(fcode, frontend=frontend)
+    expr = FindNodes(ir.Assignment).visit(routine.body)[0].rhs
+
+    # Run the identity mapper over the expression
+    new_expr = LokiIdentityMapper()(expr)
+
+    # Check that symbols and literals are equivalent, but distinct objects!
+    get_symbols = ExpressionRetriever(lambda e: isinstance(e, sym.TypedSymbol)).retrieve
+    get_literals = ExpressionRetriever(lambda e: isinstance(e, sym.IntLiteral)).retrieve
+
+    for old, new in zip(get_symbols(expr), get_symbols(new_expr)):
+        assert old == new
+        assert not old is new
+
+    for old, new in zip(get_literals(expr), get_literals(new_expr)):
+        assert old == new
+        assert not old is new
