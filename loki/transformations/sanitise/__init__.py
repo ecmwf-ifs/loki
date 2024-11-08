f9eec9fb46fb3f44ef0feaf458bf7e5261c54ff8
Michael Lange <Michael.Lange@ecmwf.int>
2024-11-08 15:10:09 +0000

    Sanitise: Add `SanitisePipeline` which provides the wider feaures

--- loki/transformations/sanitise/__init__.py
+++ loki/transformations/sanitise/__init__.py
@@ -8,14 +8,59 @@
 Sub-package with assorted utility :any:`Transformation` classes to
 harmonize the look-and-feel of input source code.
 """
-from loki.batch import Transformation
+from functools import partial
+
+from loki.batch import Transformation, Pipeline
 
 from loki.transformations.sanitise.associates import * # noqa
 from loki.transformations.sanitise.sequence_associations import * # noqa
 from loki.transformations.sanitise.substitute import * # noqa
 
 
-__all__ = ['SanitiseTransformation']
+"""
+:any:`Pipeline` class that provides combined access to the features
+provided by the following :any:`Transformation` classes, in sequence:
+1. :any:`SubstituteExpressionTransformation` - String-based generic
+expression substitution.
+2. :any:`AssociatesTransformation` - Full or partial resolution of
+nested :any:`Associate` nodes, including optional merging of
+independent association pairs.
+3. :any:`SequenceAssociationTransformation` - Resolves sequence
+association patterns in the call signature of :any:`CallStatement`
+nodes.
+
+Parameters
+----------
+substitute_expressions : bool
+    Flag to trigger or suppress expression substitution
+expression_map : dict of str to str
+    A string-to-string map detailing the substitutions to apply.
+substitute_spec : bool
+    Flag to trigger or suppress expression substitution in specs.
+substitute_body : bool
+    Flag to trigger or suppress expression substitution in bodies.
+resolve_associates : bool, default: True
+    Enable full or partial resolution of only :any:`Associate`
+    scopes.
+merge_associates : bool, default: False
+    Enable merging :any:`Associate` to the outermost possible
+    scope in nested associate blocks.
+start_depth : int, optional
+    Starting depth for partial resolution of :any:`Associate`
+    after merging.
+max_parents : int, optional
+    Maximum number of parent symbols for valid selector to have
+    when merging :any:`Associate` nodes.
+resolve_sequence_associations : bool
+    Flag to trigger or suppress resolution of sequence associations
+"""
+SanitisePipeline = partial(
+    Pipeline, classes=(
+        SubstituteExpressionTransformation,
+        AssociatesTransformation,
+        SequenceAssociationTransformation,
+    )
+)
 
 
 class SanitiseTransformation(Transformation):
--- loki/transformations/sanitise/tests/test_sanitise.py
+++ loki/transformations/sanitise/tests/test_sanitise.py
@@ -11,7 +11,9 @@ from loki import Module
 from loki.frontend import available_frontends
 from loki.ir import nodes as ir, FindNodes
 
-from loki.transformations.sanitise import SanitiseTransformation
+from loki.transformations.sanitise import (
+    SanitiseTransformation, SanitisePipeline
+)
 
 
 @pytest.mark.parametrize('frontend', available_frontends())
@@ -73,3 +75,72 @@ end module test_transformation_sanitise_mod
     calls = FindNodes(ir.CallStatement).visit(routine.body)
     assert len(calls) == 1
     assert calls[0].arguments[0] == 'a(1:3)' if resolve_sequence else 'a(1)'
+
+
+@pytest.mark.parametrize('frontend', available_frontends())
+@pytest.mark.parametrize('substitute_expressions', [True, False])
+@pytest.mark.parametrize('resolve_associates', [True, False])
+@pytest.mark.parametrize('resolve_sequence_associations', [True, False])
+def test_sanitise_pipeline(
+        tmp_path, frontend, substitute_expressions,
+        resolve_associates, resolve_sequence_associations
+):
+    """
+    Test the agglomerated :any:`SanitisePipeline` with different settings.
+    """
+    fcode = """
+module test_sanitise_pipeline_mod
+  implicit none
+
+  type rick
+    real :: scalar
+  end type rick
+contains
+
+  subroutine test_pipeline_sanitise(n, a, dave)
+    integer, intent(in) :: n
+    real, intent(inout) :: a(n+1)
+    type(rick), intent(inout) :: dave
+
+    associate(scalar => dave%scalar)
+      scalar = a(1) + a(2)
+
+      call vadd(a(1), 2.0, n+1)
+    end associate
+
+  contains
+    subroutine vadd(x, y, n)
+      real, intent(inout) :: x(n)
+      real, intent(inout) :: y
+      integer, intent(in) :: n
+
+      x = x + 2.0
+    end subroutine vadd
+  end subroutine test_pipeline_sanitise
+end module test_sanitise_pipeline_mod
+"""
+    module = Module.from_source(fcode, frontend=frontend, xmods=[tmp_path])
+    routine = module['test_pipeline_sanitise']
+
+    assoc = FindNodes(ir.Associate).visit(routine.body)
+    assert len(assoc) == 1
+    calls = FindNodes(ir.CallStatement).visit(routine.body)
+    assert len(calls) == 1
+
+    pipeline = SanitisePipeline(
+        substitute_expressions=substitute_expressions,
+        expression_map={'n + 1': 'n'},
+        resolve_associates=resolve_associates,
+        resolve_sequence_associations=resolve_sequence_associations
+    )
+    pipeline.apply(routine)
+
+    assoc = FindNodes(ir.Associate).visit(routine.body)
+    assert len(assoc) == 0 if resolve_associates else 1
+
+    calls = FindNodes(ir.CallStatement).visit(routine.body)
+    assert len(calls) == 1
+    if resolve_sequence_associations:
+        assert calls[0].arguments[0] == 'a(1:n)' if substitute_expressions else 'a(1:n+1)'
+    else:
+        assert calls[0].arguments[0] == 'a(1)'
