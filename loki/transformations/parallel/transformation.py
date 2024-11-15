ff79baa5eb01fc1974ec08575e8b2a22231af15f
Michael Lange <Michael.Lange@ecmwf.int>
2024-11-15 11:22:55 +0000

    Parallel: Process individual regions in AddViewDriverLoopTransformation

--- loki/transformations/parallel/transformation.py
+++ loki/transformations/parallel/transformation.py
@@ -5,19 +5,24 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+from loki.analyse import dataflow_analysis_attached
 from loki.batch import Transformation
+from loki.ir import (
+    nodes as ir, FindNodes, pragma_regions_attached, is_loki_pragma
+)
+from loki.types import BasicType, SymbolAttributes
 
 from loki.transformations.parallel.openmp_region import (
-    do_remove_openmp_regions, do_add_openmp_regions,
-    do_remove_firstprivate_copies,
-    do_add_firstprivate_copies,
+    do_remove_openmp_regions, do_remove_firstprivate_copies,
+    add_openmp_parallel_region, InjectFirstprivateCopyTransformer
 )
 from loki.transformations.parallel.block_loop import (
-    do_remove_block_loops, do_add_block_loops
+    do_remove_block_loops, InsertBlockLoopTransformer
 )
 from loki.transformations.parallel.field_views import (
-    do_remove_field_api_view_updates, do_add_field_api_view_updates
+    do_remove_field_api_view_updates, InsertFieldAPIViewsTransformer
 )
+from loki.transformations.utilities import ensure_imported_symbols
 
 
 __all__ = [
@@ -99,24 +104,50 @@ class AddViewDriverLoopTransformation(Transformation):
 
     def transform_subroutine(self, routine, **kwargs):
 
-        if self.add_block_loops:
-            do_add_block_loops(routine, dimension=self.dimension)
+        with pragma_regions_attached(routine):
+            for region in FindNodes(ir.PragmaRegion).visit(routine.body):
 
-        if self.add_field_api_view_updates:
-            do_add_field_api_view_updates(
-                routine, dimension=self.dimension,
-                dim_object=self.dim_object,
-                field_group_types=self.field_group_types
-            )
+                if not is_loki_pragma(region.pragma, starts_with='parallel'):
+                    continue
 
-        if self.add_firstprivate_copies:
-            do_add_firstprivate_copies(
-                routine, fprivate_map=self.fprivate_map
-            )
+                # Ensure and derive default integer type
+                ensure_imported_symbols(
+                    routine, symbols='JPIM', module='PARKIND1'
+                )
+                default_type = SymbolAttributes(
+                    BasicType.INTEGER, kind=routine.Variable(name='JPIM')
+                )
 
-        if self.add_openmp_regions:
-            do_add_openmp_regions(
-                routine, dimension=self.dimension,
-                shared_variables=self.shared_variables,
-                fprivate_variables=self.fprivate_variables
-            )
+                if self.add_block_loops:
+                    # Insert the driver block loop
+                    InsertBlockLoopTransformer(
+                        inplace=True, dimension=self.dimension,
+                        default_type=default_type
+                    ).visit(region, scope=routine)
+
+                if self.add_field_api_view_updates:
+                    # Add field group view update calls
+                    InsertFieldAPIViewsTransformer(
+                        inplace=True, dimension=self.dimension,
+                        dim_object=self.dim_object,
+                        field_group_types=self.field_group_types,
+                    ).visit(region, scope=routine)
+
+                if self.add_firstprivate_copies:
+                    # Add explicit firstprivatisation copies
+                    InjectFirstprivateCopyTransformer(
+                        inplace=True, fprivate_map=self.fprivate_map
+                    ).visit(region, scope=routine)
+
+                if self.add_openmp_regions:
+                    # Need to re-generate dataflow info here, as prior
+                    # transformers might have added new symbols.
+                    with dataflow_analysis_attached(routine):
+
+                        # Add OpenMP parallel region
+                        add_openmp_parallel_region(
+                            region=region, routine=routine,
+                            dimension=self.dimension,
+                            shared_variables=self.shared_variables,
+                            fprivate_variables=self.fprivate_variables
+                        )
