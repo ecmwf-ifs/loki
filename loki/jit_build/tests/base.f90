a9db34b37b1dc387347bba7a4062af69be15c72e
Michael Lange <Michael.Lange@ecmwf.int>
2025-03-13 10:29:46 +0000

    JIT: Workaround for f90wrap issue in build test
    For some reason, the parameter `jprb` local to the module is not
    imported from the module in the outer interfaces that f90wrap
    generates, leaving a naked symbol in one of the interface procedures.
    
    This workaround resolves this by using vanilla constants instead, but
    we should eventually extend the tests to cover this again and
    potentially report/fix this upstream.

--- loki/jit_build/tests/base.f90
+++ loki/jit_build/tests/base.f90
@@ -4,23 +4,26 @@ module base
 
   save
 
+  ! TODO: Using this in this module causes issues in f90wrap,
+  ! so we leave it here for other modules to use, but don't
+  ! use it in this module itself.
   integer, parameter :: jprb = selected_real_kind(13,300)
 
-  real(kind=jprb) :: a, b
+  real(kind=8) :: a, b
   integer :: i, j
 
 contains
 
   function a_plus_b()
     ! Test to verify module-level variables work
-    real(kind=jprb) :: a_plus_b
+    real(kind=8) :: a_plus_b
     a_plus_b = a + b
   end function a_plus_b
 
   function a_times_b_plus_c(a, b, c)
     ! Simple test to verify that module functions work
-    real(kind=jprb) :: a_times_b_plus_c
-    real(kind=jprb), intent(in) :: a, b, c
+    real(kind=8) :: a_times_b_plus_c
+    real(kind=8), intent(in) :: a, b, c
     a_times_b_plus_c = a * b + c
   end function a_times_b_plus_c
 
