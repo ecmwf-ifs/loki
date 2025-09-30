58058033713ad5db7663a62df9ef15193cf07fc5
Michael Lange <Michael.Lange@ecmwf.int>
2025-09-30 03:01:53 +0000

    Frontend: Fix type-bound procedure symbol name generation
    As shown in the added test, the `type=None` attribute can cause the
    procedure-type infor to to be lost when we clone the symbol to update
    the type-bound name of the `CallStatement`.

--- loki/frontend/fparser.py
+++ loki/frontend/fparser.py
@@ -2797,11 +2797,8 @@ class FParser2IR(GenericVisitor):
         if parent:
             scope = parent.scope
         name = self.visit(o.children[2], **kwargs)
-        # Hack: Need to force re-evaluation of the type from parent here via `type=None`
-        # To fix this, we should stop creating symbols in the enclosing scope
-        # when determining the type of drieved type members from their parent.
-        name = name.clone(name=f'{parent.name}%{name.name}', parent=parent, scope=scope, type=None)
-        return name
+        # Update the name with the type-bound parent symbol
+        return name.clone(name=f'{parent.name}%{name.name}', parent=parent, scope=scope)
 
     visit_Actual_Arg_Spec_List = visit_List
 
