8393a655795408e1814748ed0b872c9d5106b8d9
Michael Staneker <michael.staneker@ecmwf.int>
2025-10-21 06:51:59 +0000

    TemporariesPoolAllocator: stack_size_var_type -> stack_size_var_kind

--- loki/transformations/temporaries/pool_allocator.py
+++ loki/transformations/temporaries/pool_allocator.py
@@ -177,7 +177,7 @@ class TemporariesPoolAllocatorTransformation(Transformation):
         Whether to only pass the stack variable as integer to the kernel(s) or
         whether to pass the whole stack array to the driver and the calls to ``LOC()``
         within the kernel(s) itself (default: `False`)
-    stack_size_var_type: :any:`Literal` or :any:`Variable`
+    stack_size_var_kind: :any:`Literal` or :any:`Variable`
         Defaults to ``'stack_int_type_kind'``, however, can be overriden if necessary.
     """
 
@@ -192,7 +192,7 @@ class TemporariesPoolAllocatorTransformation(Transformation):
             self, block_dim, horizontal=None, stack_ptr_name='L', stack_end_name='U', stack_size_name='ISTSZ',
             stack_storage_name='ZSTACK', stack_argument_name='YDSTACK', stack_local_var_name='YLSTACK',
             local_ptr_var_name_pattern='IP_{name}', stack_int_type_kind=IntLiteral(8), directive=None,
-            check_bounds=True, cray_ptr_loc_rhs=False, stack_size_var_type=None
+            check_bounds=True, cray_ptr_loc_rhs=False, stack_size_var_kind=None
     ):
         self.block_dim = block_dim
         self.horizontal = horizontal
@@ -207,7 +207,7 @@ class TemporariesPoolAllocatorTransformation(Transformation):
         self.directive = directive
         self.check_bounds = check_bounds
         self.cray_ptr_loc_rhs = cray_ptr_loc_rhs
-        self.stack_size_var_type = stack_size_var_type or self.stack_int_type_kind
+        self.stack_size_var_kind = stack_size_var_kind or self.stack_int_type_kind
 
         if self.stack_ptr_name == self.stack_end_name:
             raise ValueError(f'"stack_ptr_name": "{self.stack_ptr_name}" and '
@@ -432,7 +432,7 @@ class TemporariesPoolAllocatorTransformation(Transformation):
 
         else:
             # Create a variable for the stack size and assign the size
-            stack_size_var_type = SymbolAttributes(BasicType.INTEGER, kind=self.stack_size_var_type)
+            stack_size_var_type = SymbolAttributes(BasicType.INTEGER, kind=self.stack_size_var_kind)
             stack_size_var = Variable(name=self.stack_size_name, type=stack_size_var_type)
 
             # Retrieve kind parameter of stack storage
