bf10b8cdb701b31c1cfb6f35dc78fd1ecd4bccec
Michael Staneker <michael.staneker@ecmwf.int>
2026-02-25 08:46:03 +0000

    add 'FindUsedVariables' expression finder

--- loki/ir/expr_visitors.py
+++ loki/ir/expr_visitors.py
@@ -29,7 +29,7 @@ __all__ = [
     'FindTypedSymbols', 'FindInlineCalls', 'FindLiterals',
     'FindRealLiterals', 'ExpressionTransformer',
     'SubstituteExpressions', 'SubstituteStringExpressions',
-    'AttachScopes'
+    'AttachScopes', 'FindUsedVariables'
 ]
 
 
@@ -182,6 +182,10 @@ class FindVariables(ExpressionFinder):
     """
     retriever = ExpressionRetriever(lambda e: isinstance(e, (Scalar, Array, DeferredTypeSymbol)))
 
+class FindUsedVariables(ExpressionFinder):
+
+    retriever = ExpressionRetriever(lambda e: isinstance(e, (Scalar, Array, DeferredTypeSymbol)),
+            recurse_var_parent=False)
 
 class FindInlineCalls(ExpressionFinder):
     """
