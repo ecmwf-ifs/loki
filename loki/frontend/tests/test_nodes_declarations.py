db8204d0e47270f2d4a140902e93327c7523f7ed
Michael Lange <Michael.Lange@ecmwf.int>
2026-04-02 09:40:57 +0000

    Frontend: Consolidate parser contract tests in frontend suites

--- loki/frontend/tests/test_nodes_declarations.py
+++ loki/frontend/tests/test_nodes_declarations.py
@@ -11,10 +11,10 @@ Verify correct frontend behaviour for declaration-like IR nodes.
 
 import pytest
 
-from loki import Module, Subroutine, BasicType
+from loki import Module, Subroutine, Function, fgen, BasicType
 from loki.jit_build import jit_compile
 from loki.expression import symbols as sym
-from loki.frontend import available_frontends, FP, HAVE_FP
+from loki.frontend import available_frontends, OMNI, REGEX, FP, HAVE_FP
 from loki.ir import nodes as ir, FindNodes
 
 
@@ -147,3 +147,228 @@ end subroutine kernel
     assert c.shape[1].lower == 0
 
     assert all('*' in str(shape) for shape in [a.shape, b.shape, c.shape])
+
+
+@pytest.mark.parametrize('frontend', available_frontends())
+def test_function_return_type(tmp_path, frontend):
+    """
+    Test various ways to define the return type of a function
+    """
+    fcode = """
+module my_funcs
+implicit none
+contains
+
+  real(kind=8) function funca(a)
+    real, intent(in) :: a
+
+    funca = a
+  end function funca
+
+  function funcb(a)
+    real(kind=8), intent(in) :: a
+    real(kind=8) :: funcb
+
+    funcb = a
+  end function funcb
+
+  function funcky(a) result(fun)
+    real, intent(in) :: a
+    real(kind=8) :: fun
+
+    fun = a
+  end function funcky
+
+  real function square(a) result(b)
+    implicit none
+    real, intent(in) :: a
+    b = a * a
+  end function square
+end module my_funcs
+    """
+    module = Module.from_source(fcode, frontend=frontend, xmods=[tmp_path])
+
+    # Implicit return type definition
+    assert isinstance(module['funca'], Function)
+    assert module['funca'].result_name == 'funca'
+    assert module['funca'].return_type.dtype == BasicType.REAL
+    assert module['funca'].return_type.kind == 8
+    assert len(FindNodes(ir.VariableDeclaration).visit(module['funca'].spec)) == 2 if frontend == OMNI else 1
+    assert len(FindNodes(ir.ProcedureDeclaration).visit(module['funca'].spec)) == 0
+
+    if frontend == OMNI:
+        # Ensure return type is declared (OMNI alwas inserts declaration)
+        fdecl = tuple(
+            d for d in FindNodes(ir.VariableDeclaration).visit(module['funca'].spec)
+            if 'funca' in d.symbols
+        )
+        assert len(fdecl) == 1 and fdecl[0].symbols[0] == 'funca'
+        assert fdecl[0].symbols[0].type.dtype == BasicType.REAL
+        assert fdecl[0].symbols[0].type.kind == 8
+    else:
+        # Check for implicit return value in `fgen`
+        fstr_header = fgen(module['funca']).splitlines()[0]
+        assert 'real(kind=8) function funca (a)' == fstr_header.lower()
+
+    # Explicit return type declaration
+    assert isinstance(module['funcb'], Function)
+    assert module['funcb'].result_name == 'funcb'
+    assert module['funcb'].return_type.dtype == BasicType.REAL
+    assert module['funcb'].return_type.kind == 8
+    assert len(FindNodes(ir.VariableDeclaration).visit(module['funcb'].spec)) == 2
+    assert len(FindNodes(ir.ProcedureDeclaration).visit(module['funcb'].spec)) == 0
+
+    # Re-named return type declaration
+    assert isinstance(module['funcky'], Function)
+    assert module['funcky'].result_name == 'fun'
+    assert module['funcky'].return_type.dtype == BasicType.REAL
+    assert module['funcky'].return_type.kind == 8
+    assert len(FindNodes(ir.VariableDeclaration).visit(module['funcky'].spec)) == 2
+    assert len(FindNodes(ir.ProcedureDeclaration).visit(module['funcky'].spec)) == 0
+
+    # Implicit return type and renamed result name
+    assert isinstance(module['square'], Function)
+    assert module['square'].result_name == 'b'
+    assert module['square'].return_type.dtype == BasicType.REAL
+    assert len(FindNodes(ir.VariableDeclaration).visit(module['square'].spec)) == 2 if frontend == OMNI else 1
+    assert len(FindNodes(ir.ProcedureDeclaration).visit(module['square'].spec)) == 0
+
+
+@pytest.mark.parametrize('frontend', available_frontends())
+def test_function_prefix(frontend):
+    """
+    Test various prefixes that can occur in function/subroutine definitions
+    """
+    fcode = """
+pure elemental real function f_elem(a)
+    real, intent(in) :: a
+    f_elem = a
+end function f_elem
+    """.strip()
+
+    routine = Function.from_source(fcode, frontend=frontend)
+    assert 'PURE' in routine.prefix
+    assert 'ELEMENTAL' in routine.prefix
+    assert isinstance(routine, Function)
+    assert routine.return_type.dtype is BasicType.REAL
+
+    assert routine.procedure_type.is_function is True
+    assert routine.procedure_type.return_type.dtype is BasicType.REAL
+    assert routine.procedure_type.procedure is routine
+
+    assert routine.procedure_symbol.type.dtype.is_function is True
+    assert routine.procedure_symbol.type.dtype.return_type.dtype is BasicType.REAL
+    assert routine.procedure_symbol.type.dtype.procedure is routine
+
+    code = fgen(routine)
+    assert 'PURE' in code
+    assert 'ELEMENTAL' in code
+
+
+@pytest.mark.parametrize('frontend', available_frontends())
+def test_function_suffix(frontend, tmp_path):
+    """
+    Test that subroutine suffixes are supported and correctly reproduced
+    """
+    fcode = """
+module subroutine_suffix_mod
+    implicit none
+
+    interface
+        function check_value(value) bind(C, name='check_value')
+            use, intrinsic :: iso_c_binding
+            real(c_float), value :: value
+            integer(c_int) :: check_value
+        end function check_value
+    end interface
+
+    interface
+        function fix_value(value) result(fixed) bind(C, name='fix_value')
+            use, intrinsic :: iso_c_binding
+            real(c_float), value :: value
+            real(c_float) :: fixed
+        end function fix_value
+    end interface
+contains
+    function out_of_physical_bounds(field, istartcol, iendcol, do_fix) result(is_bad)
+        real, intent(inout) :: field(:)
+        integer, intent(in) :: istartcol, iendcol
+        logical, intent(in) :: do_fix
+        logical :: is_bad
+
+        integer :: jcol
+        logical :: bad_value
+
+        is_bad = .false.
+        do jcol=istartcol,iendcol
+            bad_value = check_value(field(jcol)) > 0
+            is_bad = is_bad .or. bad_value
+            if (do_fix .and. bad_value) field(jcol) = fix_value(field(jcol))
+        end do
+    end function out_of_physical_bounds
+end module subroutine_suffix_mod
+    """.strip()
+    module = Module.from_source(fcode, frontend=frontend, xmods=[tmp_path])
+
+    check_value = module.interface_map['check_value'].body[0]
+    assert check_value.is_function
+    assert check_value.result_name == 'check_value'
+    assert check_value.return_type.dtype is BasicType.INTEGER
+    assert check_value.return_type.kind == 'c_int'
+    if frontend != OMNI:
+        assert check_value.bind == 'check_value'
+        assert "bind(c, name='check_value')" in fgen(check_value).lower()
+
+    fix_value = module.interface_map['fix_value'].body[0]
+    assert fix_value.is_function
+    assert fix_value.result_name == 'fixed'
+    assert fix_value.return_type.dtype is BasicType.REAL
+    assert fix_value.return_type.kind == 'c_float'
+    if frontend == OMNI:
+        assert 'result(fixed)' in fgen(fix_value).lower()
+    else:
+        assert fix_value.bind == 'fix_value'
+        assert "result(fixed) bind(c, name='fix_value')" in fgen(fix_value).lower()
+
+    routine = module['out_of_physical_bounds']
+    assert routine.is_function
+    assert routine.result_name == 'is_bad'
+    assert routine.bind is None
+    assert routine.return_type.dtype is BasicType.LOGICAL
+    assert 'result(is_bad)' in fgen(routine).lower()
+
+
+@pytest.mark.parametrize('frontend', available_frontends())
+def test_function_lazy_prefix(frontend):
+    """
+    Test that prefixes for functions are correctly captured when the object is made
+    complete.
+
+    This test represents a case where the REGEX frontend fails to capture these attributes correctly.
+
+    The rationale for this test is that we don't currently need these attributes
+    in the incomplete REGEX-parsed IR and we accept that this information is incomplete initially.
+    tmp_path, we make sure this information is captured correctly after completing the full frontend
+    parse.
+    """
+    fcode = """
+pure elemental real function f_elem(a)
+    real, intent(in) :: a
+    f_elem = a
+end function f_elem
+    """.strip()
+
+    routine = Function.from_source(fcode, frontend=REGEX)
+    assert routine._incomplete
+    assert routine.prefix == ('pure elemental real',)
+    assert routine.arguments == ()
+    assert routine.is_function is True
+    assert routine.return_type is None
+
+    routine.make_complete(frontend=frontend)
+    assert not routine._incomplete
+    assert 'PURE' in routine.prefix
+    assert 'ELEMENTAL' in routine.prefix
+    assert routine.arguments == ('a',)
+    assert routine.is_function is True
+    assert routine.return_type.dtype is BasicType.REAL
--- loki/tests/test_sourcefile.py
+++ loki/tests/test_sourcefile.py
@@ -6,108 +6,21 @@
 # nor does it submit to any jurisdiction.
 
 from pathlib import Path
-from subprocess import CalledProcessError
 import pytest
 import numpy as np
 
 from loki import (
     Sourcefile, FindNodes, PreprocessorDirective, Intrinsic,
     Assignment, Import, fgen, ProcedureType, ProcedureSymbol,
-    StatementFunction, Comment, CommentBlock, RawSource, Scalar
+    StatementFunction, Comment
 )
 from loki.jit_build import jit_compile, clean_test
-from loki.frontend import available_frontends, OMNI, FP, REGEX
+from loki.frontend import available_frontends, OMNI
 
 
 @pytest.fixture(scope='module', name='here')
 def fixture_here():
     return Path(__file__).parent
-
-
-@pytest.mark.parametrize('frontend', available_frontends())
-def test_sourcefile_properties(here, frontend, tmp_path):
-    """
-    Test that all subroutines and functions are discovered
-    and exposed via `subroutines` and `all_subroutines` properties.
-    """
-    # pylint: disable=no-member
-    filepath = here/'sources/sourcefile.f90'
-    source = Sourcefile.from_file(filepath, frontend=frontend, xmods=[tmp_path])
-    assert len(source.subroutines) == 3
-    assert len(source.all_subroutines) == 5
-
-    subroutines = ['routine_a', 'routine_b', 'function_d']
-    all_subroutines = subroutines + ['module_routine', 'module_function']
-    contained_routines = ['contained_c']
-
-    assert sum(routine.name in subroutines for routine in source.subroutines) == 3
-    assert sum(routine.name in all_subroutines for routine in source.subroutines) == 3
-    assert sum(routine.name in contained_routines for routine in source.subroutines) == 0
-
-    assert sum(routine.name in subroutines for routine in source.all_subroutines) == 3
-    assert sum(routine.name in all_subroutines for routine in source.all_subroutines) == 5
-    assert sum(routine.name in contained_routines for routine in source.all_subroutines) == 0
-
-
-@pytest.mark.parametrize('frontend', available_frontends())
-def test_sourcefile_from_source(frontend, tmp_path):
-    """
-    Test the `from_source` constructor for `Sourcefile` objects.
-    """
-    # pylint: disable=no-member
-
-    fcode = """
-! Some comment
-subroutine routine_a
-  integer a
-  a = 1
-end subroutine routine_a
-
-! Some comment
-module some_module
-contains
-  subroutine module_routine
-    integer m
-    m = 2
-  end subroutine module_routine
-  function module_function(n)
-    integer n
-    n = 3
-  end function module_function
-end module some_module
-! Other comment
-
-subroutine routine_b
-  integer b
-  b = 4
-contains
-  subroutine contained_c
-    integer c
-    c = 5
-  end subroutine contained_c
-end subroutine routine_b
-! Other comment
-
-function function_d(d)
-  integer d
-  d = 6
-end function function_d
-""".strip()
-    source = Sourcefile.from_source(fcode, frontend=frontend, xmods=[tmp_path])
-    assert len(source.subroutines) == 3
-    assert len(source.all_subroutines) == 5
-
-    subroutines = ['routine_a', 'routine_b', 'function_d']
-    all_subroutines = subroutines + ['module_routine', 'module_function']
-
-    assert [routine.name.lower() for routine in source.subroutines] == subroutines
-    assert [routine.name.lower() for routine in source.all_subroutines] == all_subroutines
-    assert 'contained_c' not in [routine.name.lower() for routine in source.subroutines]
-    assert 'contained_c' not in [routine.name.lower() for routine in source.all_subroutines]
-
-    comments = FindNodes((Comment, CommentBlock)).visit(source.ir)
-    assert len(comments) == 4
-    assert all(comment.text.strip() in ['! Some comment', '! Other comment'] for comment in comments)
 
 
 @pytest.mark.parametrize('frontend', available_frontends(xfail=[(OMNI, 'Files are preprocessed')]))
@@ -233,128 +146,6 @@ def test_sourcefile_cpp_stmt_func(here, frontend, tmp_path):
     clean_test(filepath)
 
 
-@pytest.mark.parametrize('frontend', available_frontends())
-def test_sourcefile_lazy_construction(frontend, tmp_path):
-    """
-    Test delayed ("lazy") parsing of sourcefile content
-    """
-    fcode = """
-! A comment to test
-subroutine routine_a
-integer a
-a = 1
-end subroutine routine_a
-
-module some_module
-contains
-subroutine module_routine
-integer m
-m = 2
-end subroutine module_routine
-function module_function(n)
-integer n
-n = 3
-end function module_function
-end module some_module
-
-#ifndef SOME_PREPROC_VAR
-subroutine routine_b
-integer b
-b = 4
-contains
-subroutine contained_c
-integer c
-c = 5
-end subroutine contained_c
-end subroutine routine_b
-#endif
-
-function function_d(d)
-integer d
-d = 6
-end function function_d
-    """.strip()
-    source = Sourcefile.from_source(fcode, frontend=REGEX)
-    assert len(source.subroutines) == 3
-    assert len(source.all_subroutines) == 5
-
-    some_module = source['some_module']
-    routine_b = source['routine_b']
-    module_routine = some_module['module_routine']
-    function_d = source['function_d']
-    assert function_d.arguments == ()
-
-    # Make sure we have an incomplete parse tree until now
-    assert source._incomplete
-    assert len(FindNodes(RawSource).visit(source.ir)) == 5
-    assert len(FindNodes(RawSource).visit(source['routine_a'].ir)) == 1
-
-    # Trigger the full parse
-    try:
-        source.make_complete(frontend=frontend, xmods=[tmp_path])
-    except CalledProcessError as ex:
-        if frontend == OMNI and ex.returncode == -11:
-            pytest.xfail('F_Front segfault is a known issue on some platforms')
-        raise
-    assert not source._incomplete
-
-    # Make sure no RawSource nodes are left
-    assert not FindNodes(RawSource).visit(source.ir)
-    if frontend == FP:
-        # Some newlines are also treated as comments
-        assert len(FindNodes(Comment).visit(source.ir)) == 2
-    else:
-        assert len(FindNodes(Comment).visit(source.ir)) == 1
-    if frontend == OMNI:
-        assert not FindNodes(PreprocessorDirective).visit(source.ir)
-    else:
-        assert len(FindNodes(PreprocessorDirective).visit(source.ir)) == 2
-    for routine in source.all_subroutines:
-        assert not FindNodes(RawSource).visit(routine.ir)
-        assert len(FindNodes(Assignment).visit(routine.ir)) == 1
-
-    # The previously generated ProgramUnit objects should be the same as before
-    assert routine_b is source['routine_b']
-    assert some_module is source['some_module']
-    assert module_routine is source['some_module']['module_routine']
-    assert function_d.arguments == ('d',)
-    assert isinstance(function_d.arguments[0], Scalar)
-
-
-@pytest.mark.parametrize('frontend', available_frontends())
-def test_sourcefile_lazy_comments(frontend):
-    """
-    Make sure that lazy construction can handle comments on source file level
-    (i.e. outside a program unit)
-    """
-    fcode = """
-! Comment outside
-subroutine myroutine
-    ! Comment inside
-end subroutine myroutine
-! Other comment outside
-    """.strip()
-    source = Sourcefile.from_source(fcode, frontend=REGEX)
-
-    assert isinstance(source.ir.body[0], RawSource)
-    assert isinstance(source.ir.body[2], RawSource)
-
-    myroutine = source['myroutine']
-    assert isinstance(myroutine.spec.body[0], RawSource)
-
-    source.make_complete(frontend=frontend)
-
-    assert isinstance(source.ir.body[0], Comment)
-    assert isinstance(source.ir.body[2], Comment)
-    if frontend == OMNI:
-        assert isinstance(myroutine.body.body[0], Comment)
-    else:
-        assert isinstance(myroutine.docstring[0], Comment)
-
-    code = source.to_fortran()
-    assert '! Comment outside' in code
-    assert '! Comment inside' in code
-    assert '! Other comment outside' in code
 
 
 @pytest.mark.parametrize('frontend', available_frontends(include_regex=True))
