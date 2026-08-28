43ab81654911d750462c416d2a393b9ef2a4c829
Michael Lange <Michael.Lange@ecmwf.int>
2026-08-28 07:09:44 +0000

    Loki: Make all license headers machine-readable via SPDX annotations

--- loki/ir/tests/test_pragma_utils.py
+++ loki/ir/tests/test_pragma_utils.py
@@ -1,3 +1,9 @@
+# SPDX-FileCopyrightText: 2018 European Centre for Medium-Range Weather Forecasts (ECMWF)
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileComment: In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
 from io import StringIO
 import pytest
 
